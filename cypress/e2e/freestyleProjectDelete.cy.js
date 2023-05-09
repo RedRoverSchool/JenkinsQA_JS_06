b53fda765346575b0c31980df8d9fa00610830f2
MaryMoroz <morozova.mary@gmail.com>
2023-05-09 00:05:26 -0400

    change describe name

--- cypress/e2e/freestyleProjectDelete.cy.js
+++ cypress/e2e/freestyleProjectDelete.cy.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress"/>
 import messages from "../fixtures/messages.json"
 
-describe('Freestyle project> Delete created project', () => {
+describe('Freestyle project. Delete created project', () => {
     
     it('Delete project using dropdown menu', function () {
       cy.get('a[href="newJob"]').click()
