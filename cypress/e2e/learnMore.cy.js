0aef6ab9c348bc5358b41d6cdbf552596eb520a0
Kiryl Makhveyenia <mahveenya.k.a@gmail.com>
2023-05-10 17:07:34 +0300

    AT_02.05_002 | Redirection to wiki

---
+++ cypress/e2e/learnMore.cy.js
@@ -0,0 +1,14 @@
+describe("AT_02.05_002 | Homepage Link 'Learn more about distributed builds'", () => {
+  it("Redirection to wiki occurs after clicking the 'Learn more about distributed builds' link", () => {
+    cy.intercept('GET', '**/Distributed+builds').as('reqToWiki');
+
+    cy.get('a[href*=distributed-builds]')
+      .should('have.attr', 'target', '_blank')
+      .invoke('removeAttr', 'target')
+      .click();
+
+    cy.wait('@reqToWiki').then((xhr) => {
+      expect(xhr.response.statusCode).to.equal(200);
+    });
+  });
+});
