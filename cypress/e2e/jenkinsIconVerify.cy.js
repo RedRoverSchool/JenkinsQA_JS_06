f930e016f9caae41f5f9181e8d224badfb48d1e8
EkBlinova <katte.blinova@gmail.com>
2023-05-10 08:56:12 -0700

    01.01_001 | <Header> 2 commit

--- cypress/e2e/jenkinsIconVerify.cy.js
+++ cypress/e2e/jenkinsIconVerify.cy.js
@@ -1,12 +1,12 @@
 /// <reference types="cypress"/>
 
-describe('Header: Jenkins icon', () => {
+describe('Header', () => {
 
     it('Verify Jenkins icon', function () {
 
         cy.get('#jenkins-head-icon').should('be.visible')
         cy.get('#jenkins-head-icon').click()
-        cy.url().should('eq', 'http://localhost:8080/')
+        cy.url().should('contain', 'localhost')
 
         cy.get('#jenkins-head-icon').should('have.css', 'height', '40px')
         cy.get('#jenkins-head-icon').should('have.css', 'vertical-align', 'middle')
