bf1a0316371af1dd89c8f633f16dc289aa9054fc
Artyom Dulya <fecc1612@gmail.com>
2023-05-10 23:38:05 +0500

    AT_5.05 | <New Item > Create a new Multibranch Pipeline

--- cypress/e2e/newItemTest.cy.js
+++ cypress/e2e/newItemTest.cy.js
@@ -8,11 +8,24 @@ describe("newItemTest", () => {
         cy.get("[class='add-item-name'] input[name='name']").type(`Engineer `);
         cy.contains("Pipeline").click();
         cy.get("#ok-button").click();
-        cy.get("textarea[name='description']").type("new create super project");
         cy.get("button[name='Submit']").click();
         cy.get("li:nth-child(1) > a").click();
 
         cy.get("[href$='Engineer/']").should("have.text", "Engineer");
+    });
+
+    it("<New Item > Create a new Multibranch Pipeline", () => {
+        cy.get("#side-panel").click();
+        cy.contains("New Item").click();
+        cy.get("[class='add-item-name'] input[name='name']").type(`Engineer `);
+        cy.contains("Multibranch Pipeline").click();
+        cy.get("#ok-button").click();
+        cy.get("button[name='Submit']").click();
+        cy.get("#breadcrumbs > :nth-child(1) > .model-link").click();
+
+        cy.get("[href$='Engineer/']").should("have.text", "Engineer");
+        cy.get("[tooltip='Multibranch Pipeline']").should("be.visible")
+        
     });
 
 });
