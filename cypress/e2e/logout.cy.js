30af214441785d61682b97f016c058f14fdeecf8
Larysa Reznikova <lorrez@gmail.com>
2023-05-10 20:02:34 -0500

    verification log out button

---
+++ cypress/e2e/logout.cy.js
@@ -0,0 +1,7 @@
+describe('verification log out button', function () {
+
+    it('At_01_08_001 log out button should be clickable and open login page', function () {
+        cy.get('a[href="/logout"]').click()
+        cy.url().should('contain','http://localhost:8080/login')
+    });
+});
