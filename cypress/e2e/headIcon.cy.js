1ec8dcb3fb4108c15c476a6c09ed4279b15611a2
SiarheiMSV <melnichak35@gmail.com>
2023-05-11 09:29:50 -0400

    AT_01.02.002_Check_search_box

--- cypress/e2e/headIcon.cy.js
+++
@@ -1,9 +0,0 @@
-/// <reference types="cypress" />
-
-describe('Head Icon', () => {
-
-    it('Verify Jenkins icon redirection to the homepage', () => {
-        cy.get('#jenkins-head-icon').click()
-        cy.url().should('include', 'localhost')
-    })
-})
