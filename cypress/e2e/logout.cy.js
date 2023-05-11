9c0d8fb3bcb11749cb0632d76d7fb0d19dbcf5e1
Larysa Reznikova <lorrez@gmail.com>
2023-05-10 21:23:53 -0500

    verification log out button

--- cypress/e2e/logout.cy.js
+++ cypress/e2e/logout.cy.js
@@ -1,6 +1,6 @@
 describe('verification log out button', function () {
 
-    it('AT_01_08_001 log out button should be clickable and open login page', function () {
+    it('At_01_08_001 log out button should be clickable and open login page', function () {
         cy.get('a[href="/logout"]').click()
         cy.url().should('contain','http://localhost:8080/login')
     });
