977eaf020cfaa037f110016ad195f7070302db0e
Kiryl Makhveyenia <mahveenya.k.a@gmail.com>
2023-05-11 12:01:31 +0300

    refactor: change describe and tc name

--- cypress/e2e/homepage02.05_002.cy.js
+++ cypress/e2e/homepage02.05_002.cy.js
@@ -1,5 +1,5 @@
-describe("Homepage", () => {
-  it("Redirection to wiki occurs after clicking the 'Learn more about distributed builds' link", () => {
+describe("US_02.05 | <Homepage> Link 'Learn more about distributed builds'", () => {
+  it("AT_02.05_002 | Redirection to wiki occurs after clicking the 'Learn more about distributed builds' link", () => {
     cy.intercept('GET', '**/Distributed+builds').as('reqToWiki');
 
     cy.get('a[href*=distributed-builds]')
