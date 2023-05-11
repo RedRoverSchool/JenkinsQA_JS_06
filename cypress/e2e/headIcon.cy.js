3f43d1612c886925fa3795ed3ea822f9a625ca3b
Andriy Danilevskyy <andriy.danilevskyy@gmail.com>
2023-05-11 13:15:06 +0300

    Head icon visibility test

--- cypress/e2e/headIcon.cy.js
+++ cypress/e2e/headIcon.cy.js
@@ -2,6 +2,11 @@
 
 describe('Head Icon', () => {
 
+    it('Verify Jenkins icon and name-icon are visible in header', () => {
+        cy.get('#jenkins-home-link').should("be.")
+        cy.get('#jenkins-name-icon').should("be.visible")
+    })
+
     it('Verify Jenkins icon redirection to the homepage', () => {
         cy.get('#jenkins-head-icon').click()
         cy.url().should('include', 'localhost')
