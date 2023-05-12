5fec97a9b08936d35e4a3254f8f1eb94aeea6e6b
Solovey-PO <solovey.peter@gmail.com>
2023-05-11 19:47:45 -0600

    remove only

--- cypress/e2e/adminDropDownMenu.cy.js
+++ cypress/e2e/adminDropDownMenu.cy.js
@@ -3,7 +3,7 @@
 
 describe('US 17.04 | Page-header', () => {
 
-    it.only('TC 17.04 | Open "My views" tab from user dropdown', () => {
+    it('TC 17.04 | Open "My views" tab from user dropdown', () => {
         cy.get('.page-header__hyperlinks .model-link .jenkins-menu-dropdown-chevron').realHover().click()
         cy.get('.yuimenuitem').contains('My Views').click()
         cy.url().should('contain', 'view/all/')
