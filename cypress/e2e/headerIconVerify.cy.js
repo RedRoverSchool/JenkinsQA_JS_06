b9b15bb21a114a989de992025b299bb77c55aed9
Milla Kaidalova <milladiz84@gmail.com>
2023-05-11 21:38:22 -0400

    Change describe name

--- cypress/e2e/headerIconVerify.cy.js
+++ cypress/e2e/headerIconVerify.cy.js
@@ -1,9 +1,8 @@
 /// <reference types="cypress"/>
 
-describe('<Header> Head Icon', () => {
+describe('Header | Head Icon', () => {
     
-    it('Verify Jenkins icon and name-icon', function () {
-        cy.get('#jenkins-head-icon').should('be.visible');
-        cy.get('#jenkins-name-icon').should('be.visible');
+    it('Verify Jenkins icon', function () {
+        cy.get('#jenkins-head-icon').should('be.visible');        
     });
 })
