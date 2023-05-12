3b6eca86f962b3e2217103228d57b4a2aa2c4d1a
g.futorka@gmail.com <Galustyn3009>
2023-05-12 15:40:34 -0400

    AT_16.02_002 | Multibranch Pipeline Rename_negativeScenario1

--- cypress/e2e/multibranchPipelineRename.cy.js
+++ cypress/e2e/multibranchPipelineRename.cy.js
@@ -24,4 +24,14 @@ describe('Rename existing Multibranch Pipeline', () =>{
         cy.url().should('include', `/job/${pipelineName.newNamePipeline}`);
         cy.get('h1').contains(`${pipelineName.newNamePipeline}`);
     })
+
+    it('Rename Multibranch Pipeline using dropdown menu_negative1', () => {
+        cy.get('a[href^="job/"').realHover();
+        cy.get('td > a [class$="dropdown-chevron"]').click();
+        cy.get('li > a > span').contains('Rename').click();
+        cy.get('button[name=Submit]').click();
+        cy.url().should('include', `/job/${pipelineName.namePipeline}/confirmRename`);
+        cy.get('#main-panel h1').should('have.text', messages.renameErrorMessage.error);
+        cy.get('#main-panel p').should('have.text', messages.renameErrorMessage.message);
+    })
 })
