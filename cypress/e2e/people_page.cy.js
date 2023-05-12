58e4d060abaa2eea4184b664c1e8736ebeb58b58
Parizat Ismailova <parizat.ismailova.kk@gmail.com>
2023-05-12 17:40:34 -0500

    refactorTest

--- cypress/e2e/people_page.cy.js
+++ cypress/e2e/people_page.cy.js
@@ -2,7 +2,8 @@
 
 describe('AT_6.01_003 | <People> Verify the "People" page', ()=>{
     it('Verify the "People" page', ()=>{
-        cy.get('a[href="/asynchPeople/"]').should('exist').should('have.text','People').click()
+        cy.get('#side-panel #tasks :nth-child(2) .task-link-text').should('exist').should('have.text','People')
+        cy.get('a[href="/asynchPeople/"]').click()
         cy.url().should('includes','/asynchPeople/')
     })
 })
