97fe5ce75107c4d21df55ba09316bc9d7cd3129c
Tatiana BM <burykina.tatiana@gmail.com>
2023-05-13 01:00:03 +0200

    change it name from TC to AT

--- cypress/e2e/peopleTabAndPage.cy.js
+++ cypress/e2e/peopleTabAndPage.cy.js
@@ -6,7 +6,7 @@ describe("People Tab and People page", () => {
     cy.get("a.task-link").eq(1).should("contain", "People").click();
     cy.url().should("contains", endPointPeople);
   });
-  it('TC_6.01_004|Verify URL the user is being redirected to', () => {
+  it('AT_6.01_004|Verify URL the user is being redirected to', () => {
     cy.get('a[href="/asynchPeople/"]').click()
     cy.url().should('contain', '/asynchPeople/')
   })
