dab81df2d3597bec41e2cee55cdad05321915f1a
IrinaVorontsova1 <iraavorontsova@gmail.com>
2023-05-12 21:09:14 -0700

    AT_17.03_004

--- cypress/e2e/orgFolderDelete.cy.js
+++ cypress/e2e/orgFolderDelete.cy.js
@@ -14,4 +14,19 @@ describe('<Organization Folder> Delete Organization Folder', () => {
         cy.get('div#breadcrumb-menu ul li a').contains('Delete Organization Folder').click();
         cy.get('button[name="Submit"]').click()
     });
+
+    it('AT_17.03_004 | Organization Folder>Delete Organization Folder', () => {
+        cy.get('a[href="newJob"]').click()
+        cy.get('input[name="name"]').type('project')
+        cy.get('span.label').contains('Organization Folder').click()
+        cy.get('#ok-button').click()
+        cy.get('button[name="Submit"]').click()
+        cy.get(':nth-child(1) > .model-link').click()
+        cy.get('[alt="Organization Folder"]').should('be.visible')
+
+        cy.get('tbody tr td a.jenkins-table__link').realHover()
+        cy.get('table#projectstatus button.jenkins-menu-dropdown-chevron').should('be.visible').click()
+        cy.get('div#breadcrumb-menu ul li a').contains('Delete Organization Folder').click();
+        cy.get('button[name="Submit"]').click()
+    })
 });
