4ee6e18fb7233dce88cee237c9f98f3774badb4f
LanaBondarenko <bondarenkosvitlana22@gmail.com>
2023-05-13 16:03:55 -0400

    Refactor Test

--- cypress/e2e/logOutButtonVerifying.cy.js
+++ cypress/e2e/logOutButtonVerifying.cy.js
@@ -2,10 +2,7 @@
 
 describe('Header Log out button', () => {
   it('Verify logout button is clickable and redirects to login page', function () {
-    cy.get('a[href="/logout"]').click();
-    cy.url().should('contain', 'login')
-    cy.get('#j_username').should('be.visible').and('have.attr', 'placeholder', 'Username');
-    cy.get(':nth-child(2) > .jenkins-input').should('be.visible').and('have.attr', 'placeholder', 'Password');
-    cy.get('.submit button').contains('Sign in').should('be.visible');
+    cy.get('a[href="/logout"]').should('be.visible').and('have.text','log out').click();
+    cy.url().should('contain', 'login');    
   });      
 });
