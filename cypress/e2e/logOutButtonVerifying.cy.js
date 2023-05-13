b8ae7da499953244359df9b32caa8f58da7d7b5f
LanaBondarenko <bondarenkosvitlana22@gmail.com>
2023-05-13 15:52:51 -0400

    Refactor Test

--- cypress/e2e/logOutButtonVerifying.cy.js
+++ cypress/e2e/logOutButtonVerifying.cy.js
@@ -1,15 +1,11 @@
 /// <reference types="cypress"/>
 
 describe('Header Log out button', () => {
-    it('Verify logout button is visible', function () {
-      cy.get('a[href="/logout"]').should('be.visible').and('have.text','log out');
-    });
-
-    it('Verify logout button is clickable and redirects to login page', function () {
-        cy.get('a[href="/logout"]').click();
-        cy.url().should('contain', 'login')
-        cy.get('#j_username').should('be.visible').and('have.attr', 'placeholder', 'Username');
-        cy.get(':nth-child(2) > .jenkins-input').should('be.visible').and('have.attr', 'placeholder', 'Password');
-        cy.get('.submit button').contains('Sign in').should('be.visible');
-      });      
-  });
+  it('Verify logout button is clickable and redirects to login page', function () {
+    cy.get('a[href="/logout"]').click();
+    cy.url().should('contain', 'login')
+    cy.get('#j_username').should('be.visible').and('have.attr', 'placeholder', 'Username');
+    cy.get(':nth-child(2) > .jenkins-input').should('be.visible').and('have.attr', 'placeholder', 'Password');
+    cy.get('.submit button').contains('Sign in').should('be.visible');
+  });      
+});
