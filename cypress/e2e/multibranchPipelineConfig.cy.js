e7952b9d2adb0442b54de6a51a6881cace1bbe6b
Yulia Lopatko <engineeryulia@gmail.com>
2023-05-12 22:29:34 -0600

    add TC_16.01.001

---
+++ cypress/e2e/multibranchPipelineConfig.cy.js
@@ -0,0 +1,19 @@
+/// <reference types="cypress"/>
+
+describe('Configurate Multibranch Pipeliner', () => {
+
+  beforeEach('Create multibranch pipeline', function () {
+      cy.get('a[href="newJob"]').click();
+      cy.get('#name').type('job');
+      cy.contains('Multibranch Pipeline').click();
+      cy.get('#ok-button').click();
+      cy.get('button[name=Submit]').click();
+  });
+
+  it('Create "job-1" configuration', () => {
+      cy.get('[href="/job/job/configure"]').click();
+      cy.get('.jenkins-input.validated').type('job-1')
+      cy.get('[name="_.description"]').type('first job');
+      cy.get('button[name=Submit]').click();
+  });
+})
