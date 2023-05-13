f0ba6ca7ee92e93f5e0c519320b5f266dd83c1f3
Paul-Korz <paulkorzenok@gmail.com>
2023-05-13 12:31:06 +0300

    refactor test

--- cypress/e2e/header.cy.js
+++ cypress/e2e/header.cy.js
@@ -1,10 +1,12 @@
 /// <reference types="cypress" />
 
-describe('<Header> Head Icon', ()=>{
+describe('Header | Head Icon', ()=>{
 
     it('Verify Jenkins Icon Functionality', function(){
+    cy.get('span[class="task-link-text"]').contains('People').click({force: true})
     cy.get('#jenkins-name-icon').should('be.visible')
-    cy.get('#jenkins-name-icon').click()
+    cy.get('#jenkins-head-icon').should('be.visible')
+    cy.get('#jenkins-name-icon').click('')
     cy.get('.empty-state-block > h1').should('contain', 'Welcome to Jenkins!')
     })
 })
