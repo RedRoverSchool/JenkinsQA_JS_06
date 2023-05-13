7d2332135b993ec48742cd587dc6af72037a95eb
leshajerm <leshajerm@gmail.com>
2023-05-13 12:03:12 -0400

    AT_01.01_007 | Verify Head Icon

---
+++ cypress/e2e/VerifyHeadIcon.cy.js
@@ -0,0 +1,10 @@
+/// <reference types="cypress"/>
+
+describe("Header | Head Icon", () => {
+
+    it("Verify Head Icon", () => {
+        cy.visit('http://localhost:8080/asynchPeople/')
+        cy.get("#jenkins-name-icon").click()
+        cy.url().should('include', 'localhost')
+    })
+})
