f7b7b9a72800bba5d25fe0d6028b422a89af9b7d
Tatsiana Nazarava <t.nazarova.qa@gmail.com>
2023-05-13 12:46:04 +0300

    AT_05.07 _002 | Verify +New Item_inputField

--- cypress/e2e/newItemInputField.cy.js
+++ cypress/e2e/newItemInputField.cy.js
@@ -7,4 +7,10 @@ describe('New Item', () => {
         cy.url().should('include', '/view/all/newJob')
         cy.get('.jenkins-input').should('be.visible')
     })
+    
+    it('AT_05.07 _002 | Verify +New Item button is clickable on the Main page', () => {
+        cy.get('a[href ="/view/all/newJob"]').click()
+        cy.url().should('eq', 'http://localhost:8080/view/all/newJob')
+        cy.get('#name').should('be.visible')
+    })
 })
