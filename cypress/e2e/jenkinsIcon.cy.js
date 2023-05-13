487a8c5dd8fa5ecbc0cc3e7e45e51b6d008603ac
VadimMotorov <vadm310776@gmal.com>
2023-05-13 13:58:51 +0300

    AT_01.01.020| Check the Jenkins icon

--- cypress/e2e/jenkinsIcon.cy.js
+++ cypress/e2e/jenkinsIcon.cy.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress"/>
 
 describe('Header - head icon', () => {
-    it('verify jenkins icon', function () {
+    it.only('verify jenkins icon', function () {
       cy.get('a[href="/view/all/newJob"]').click();
       cy.get('#jenkins-name-icon').click();
       cy.get('div h1').should('be.visible');
