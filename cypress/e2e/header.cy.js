11669ca4e9ec06b275bb794d46f1ab654bdb0722
Paul-Korz <paulkorzenok@gmail.com>
2023-05-13 15:09:22 +0300

    refactor

--- cypress/e2e/header.cy.js
+++ cypress/e2e/header.cy.js
@@ -4,9 +4,8 @@ describe('Header | Head Icon', ()=>{
 
     it('Verify Jenkins Icon Functionality', function(){
     cy.get('span[class="task-link-text"]').contains('People').click({force: true})
-    cy.get('#jenkins-name-icon').should('be.visible')
-    cy.get('#jenkins-head-icon').should('be.visible')
-    cy.get('#jenkins-name-icon').click('')
+    cy.get('#jenkins-home-link').should('be.visible')
+    cy.get('#jenkins-home-link').click('')
     cy.get('.empty-state-block > h1').should('contain', 'Welcome to Jenkins!')
     })
 })
