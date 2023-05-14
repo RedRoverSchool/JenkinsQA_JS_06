50c2272a5416f954facc5059c427818f50f69ebb
DimaCurkanu <kdgrig@gmail.com>
2023-05-14 14:40:08 -0500

    AT_12.07_001 | fixed assert

--- cypress/e2e/freestyleProjectAddDescription.cy.js
+++ cypress/e2e/freestyleProjectAddDescription.cy.js
@@ -3,7 +3,7 @@
 describe('Verify Add Description click', () => {
   it('verify clickable', function () {
     cy.get('.leading-icon').realHover();
-    cy.get('#description-link').should('be.visible').and('include.text', 'Add description').click();
+    cy.get('#description-link').should('be.visible').and('includes.text', 'description').click();
     cy.get('.jenkins-input').type('Success');
     cy.get('.jenkins-input').should('be.visible');
   });
