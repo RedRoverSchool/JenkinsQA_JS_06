fd50f7102ea812068bc2dab82327822da257741c
Aram_H <63616812+AramH20@users.noreply.github.com>
2023-05-13 22:54:10 -0700

    Update OrganizationFolderRename.cy.js

--- cypress/e2e/OrganizationFolderRename.cy.js
+++ cypress/e2e/OrganizationFolderRename.cy.js
@@ -4,8 +4,7 @@ import organizationFolderNames  from "../fixtures/organizationFolderNames.json";
 
 describe('Rename existing Organization Folder', () => {
     beforeEach(() => {
-        cy.get('a[href$="/newJob"]').click();
-        // cy.get('.jenkins-input').as('nameInputField');
+        cy.get('a[href$="/newJob"]').click()
         cy.get('.jenkins-input').type(organizationFolderNames.nameOrganizationFolder);
         cy.get('[class="label"]').contains('Organization Folder').click();
         cy.get('button[type=submit]').should('be.enabled').click();
@@ -18,11 +17,9 @@ describe('Rename existing Organization Folder', () => {
         cy.get('a[href^="job/"').realHover();
         cy.get('td > a [class$="dropdown-chevron"]').click();
         cy.get('li > a > span').contains('Rename').click();
-        cy.get('.jenkins-input').clear();
-        cy.get('.jenkins-input').type(organizationFolderNames.newOrganizationFolder);
+        cy.get('.jenkins-input').clear().type(organizationFolderNames.newOrganizationFolder);
         cy.get('button[name=Submit]').click();
         cy.url().should('include', `/job/${organizationFolderNames.newOrganizationFolder}`);
         cy.get('h1').contains(`${organizationFolderNames.newOrganizationFolder}`);
     })
-
 })
