b50a7ec3323c5bf584f26d9d3f16b71e679295ec
Alexeeva Viktoria <tori-r@mail.ru>
2023-05-14 10:02:15 +0200

    AT_02.01_03_error correction

--- cypress/e2e/homepageCreate.cy.js
+++ cypress/e2e/homepageCreate.cy.js
@@ -1,8 +1,7 @@
 /// <reference types="cypress"/>
 
-describe('<Homepage> Create a job link', () => {
+describe('Homepage Create a job link', () => {
     it('AT_02.01_003 | Create a job link', () => {
-        cy.visit('http://localhost:8080/');
         cy.get('a[href="newJob"]').should('be.visible').click();
         cy.url().should('contain', '/newJob')
     })
