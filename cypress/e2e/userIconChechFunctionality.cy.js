2d1ab05739802105eac2673b2217819dffc1a2f3
DariaMas7 <masiuk.daria1994@gmail.com>
2023-05-15 12:49:54 -0700

    refactorTest

--- cypress/e2e/userIconChechFunctionality.cy.js
+++ cypress/e2e/userIconChechFunctionality.cy.js
@@ -1,32 +1,24 @@
 /// <reference types="cypress"/>
 
 describe('Header | User icon', () => {
-    it('The user icon is visible and clickable', () => {
-        cy.get('[href="/user/dariamas"]').should('exist').click()
+    it('The user icon and dropdown list', () => {
+        cy.get('[class="login page-header__hyperlinks"] .model-link').should('exist').click()
         cy.get('.icon-lg').should('exist')
-    })
-    it('Check dropdown | Builds option', ()=>{
         cy.get('#page-header .jenkins-menu-dropdown-chevron').click({force: true})
         cy.get('#breadcrumb-menu').should('exist')
-        cy.get('[href="/user/dariamas/builds"]').click()
+        cy.get('.yuimenuitemlabel span').contains('Builds').click()
         cy.get('[name=skip2content]').should('exist')
-    })
-    it('Check dropdown | Configure option', ()=>{
         cy.get('#page-header .jenkins-menu-dropdown-chevron').click({force: true})
         cy.get('#breadcrumb-menu').should('exist')
-        cy.get('[href="/user/dariamas/configure"]').click()
+        cy.get('.yuimenuitemlabel span').contains('Configure').click()
         cy.url().should('includes','/user/dariamas/configure')
-    })
-    it('Check dropdown | My views option', ()=>{
         cy.get('#page-header .jenkins-menu-dropdown-chevron').click({force: true})
         cy.get('#breadcrumb-menu').should('exist')
-        cy.get('[href="/user/dariamas/my-views"]').click()
+        cy.get('.yuimenuitemlabel span').contains('My Views').click()
         cy.url().should('includes','/user/dariamas/my-views/view/all/')
-    })
-    it('Check dropdown | Credential option', ()=>{
         cy.get('#page-header .jenkins-menu-dropdown-chevron').click({force: true})
         cy.get('#breadcrumb-menu').should('exist')
-        cy.get('[href="/user/dariamas/credentials"]').click()
+        cy.get('.yuimenuitemlabel span').contains('Credentials').click()
         cy.get('.jenkins-app-bar__content').should('exist')
     })
 })
