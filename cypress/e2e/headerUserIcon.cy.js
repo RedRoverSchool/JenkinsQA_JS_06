4231105e1664fac3df09571729172c9102c95522
SvetlanaKazakova <mks.kazakova@mail.ru>
2023-05-15 11:51:55 +0300

    TC_01.03 _006| Header>User icon

--- cypress/e2e/headerUserIcon.cy.js
+++ cypress/e2e/headerUserIcon.cy.js
@@ -6,7 +6,7 @@ describe('Header User Icon', () => {
     cy.get('.login .model-link').should('be.visible');
     });
 
-    it('AT_01.03_003| Header | User icon | Verification of the visibility of the dropdown menu', function () {
+    xit('AT_01.03_003| Header | User icon | Verification of the visibility of the dropdown menu', function () {
         cy.get('a[href= "/user/admin"] button').click({force:true})
         cy.get('ul.first-of-type').should('be.visible')
     })
@@ -15,10 +15,25 @@ describe('Header User Icon', () => {
         cy.get('.login.page-header__hyperlinks .model-link').should('be.visible');
     });
 
-    it('AT_01.03_004| Header | User icon | Ability to choose one of the menu-list option by clicking on it', function () {
+    xit('AT_01.03_004| Header | User icon | Ability to choose one of the menu-list option by clicking on it', function () {
         cy.get('a[href= "/user/admin"] button').click({force:true})
         cy.get('ul.first-of-type').should('be.visible')
         cy.get('ul.first-of-type span').contains('Builds').click()
         cy.url().should('contain', '/user/admin/builds')
+    })
+
+    it('TC_01.03 _006| Header>User icon',()=>{
+        cy.get('.login .jenkins-menu-dropdown-chevron').click({force:true})
+        cy.get('.first-of-type span').contains('Builds').click()
+        cy.url().should('contain','/user/svetlana/builds')
+        cy.get('.login .jenkins-menu-dropdown-chevron').click({force:true})
+        cy.get('.first-of-type span').contains('Configure').click()
+        cy.url().should('contain','/user/svetlana/configure')
+        cy.get('.login .jenkins-menu-dropdown-chevron').click({force:true})
+        cy.get('.first-of-type span').contains('My Views').click()
+        cy.url().should('contain','user/svetlana/my-views/view/all/')
+        cy.get('.login .jenkins-menu-dropdown-chevron').click({force:true})
+        cy.get('.first-of-type span').contains('Credentials').click()
+        cy.url().should('contain','/user/svetlana/credentials/')
     })
 })
