683b8d7b28d713988f12555320bd909d80f79d5d
Halyna A <7108581@gmail.com>
2023-05-15 12:21:45 -0700

    AT_01.04_006|Verify the side panel

--- cypress/e2e/headerUserBuildsLink.cy.js
+++ cypress/e2e/headerUserBuildsLink.cy.js
@@ -16,4 +16,18 @@ describe('Header - User Builds Link', () => {
           cy.get('h1').should('have.text', 'Builds for ' + userName)
         })
   }); 
+
+it('AT_01.04_006| Verify the side panel', () =>{
+  let userBuilds = 'a[href="/user/admin/builds"].yuimenuitemlabel'
+  let dropDown = 'button:nth-child(3)'
+    cy.get(dropDown).click( {force: true});
+    cy.get(userBuilds).click();
+    cy.get('#side-panel').should('be.visible')
+ });
+
 });
+
+
+
+
+
