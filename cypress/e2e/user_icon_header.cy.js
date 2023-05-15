e2b9443e9d5735dc3eece443e7485225da583a6e
Parizat Ismailova <parizat.ismailova.kk@gmail.com>
2023-05-15 13:46:56 -0500

    AT_1.03_008 | <Header> Verify the "User" icon

---
+++ cypress/e2e/user_icon_header.cy.js
@@ -0,0 +1,30 @@
+/// <reference types="cypress"/>
+ describe('<Header |User icon', ()=>{
+    it('Verify the "User" icon is clickable', ()=>{
+        cy.get('a[href="/user/peri"]').click()
+        cy.url('').should('includes','/user/peri/')
+        cy.get('a[href="/user/peri"]').click()
+        cy.get('.page-header .jenkins-menu-dropdown-chevron').click({force:true})
+        cy.get('#yui-gen1 a[href="/user/peri/builds"]').click()
+        cy.url().should('includes','/user/peri/builds')
+
+        cy.get('a[href="/user/peri"]').click()
+        cy.get('.page-header .jenkins-menu-dropdown-chevron').click({force:true})
+        cy.get('a[href="/user/peri/configure"].yuimenuitemlabel').click()
+        //cy.contains('Configure').click()
+        cy.url().should('includes','/user/peri/configure')
+        
+        cy.get('a[href="/user/peri"]').click()
+        cy.get('.page-header .jenkins-menu-dropdown-chevron').click({force:true})
+        cy.get('a[href="/user/peri/my-views"].yuimenuitemlabel').click()
+        //cy.contains('My Views').click()
+        cy.url().should('includes','/user/peri/my-views/view/all/')
+        
+        cy.get('a[href="/user/peri"]').click()
+        cy.get('.page-header .jenkins-menu-dropdown-chevron').click({force:true})
+        cy.get('a[href="/user/peri/credentials"].yuimenuitemlabel').click()
+        //cy.contains('Credentials').click()
+        cy.url().should('includes','/user/peri/credentials/')
+        
+    })
+ })
