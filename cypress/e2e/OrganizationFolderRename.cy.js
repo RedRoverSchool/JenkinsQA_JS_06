90f643a6144c75b7b98bf511de1b83fe4b3ca6ba
Ekaterina Romanova <e.catherrina@gmail.com>
2023-05-16 02:05:01 +0300

    Restore test

--- cypress/e2e/OrganizationFolderRename.cy.js
+++ cypress/e2e/OrganizationFolderRename.cy.js
@@ -12,7 +12,7 @@ describe('Rename existing Organization Folder', () => {
         cy.url().should('include', `/${organizationFolderNames.nameOrganizationFolder}/configure`);
         cy.get('button[name=Submit]').click();
         cy.get('[class="model-link"]').contains('Dashboard').click();
-    })
+    });
 
     it('Rename Organization Folder using dropdown menu_positive', ()=> {
         cy.get('a[href^="job/"').realHover();
@@ -20,9 +20,20 @@ describe('Rename existing Organization Folder', () => {
         cy.get('li > a > span').contains('Rename').click();
         cy.get('.jenkins-input').clear().type(organizationFolderNames.newOrganizationFolder);
         cy.get('button[name=Submit]').click();
+
         cy.url().should('include', `/job/${organizationFolderNames.newOrganizationFolder}`);
         cy.get('h1').contains(`${organizationFolderNames.newOrganizationFolder}`);
-    })
+    });
+
+    it('Rename Organization Folder using left side menu_positive', ()=> {
+        cy.get('a[href^="job/"').click()
+        cy.get('#tasks > div:nth-child(8)>span').contains('Rename').click()
+        cy.get('.jenkins-input').clear().type(organizationFolderNames.newOrganizationFolder);
+        cy.get('button[name=Submit]').click();
+
+        cy.url().should('include', `/job/${organizationFolderNames.newOrganizationFolder}`);
+        cy.get('h1').contains(`${organizationFolderNames.newOrganizationFolder}`);
+    });
 
     it('Rename Organization Folder with the same name', () => {
         cy.get('[href^="job"]').realHover();
@@ -34,4 +45,4 @@ describe('Rename existing Organization Folder', () => {
             expect($p.text().trim()).equal(messages.renameErrorMessage.message);
           });
     });
-});
+})
