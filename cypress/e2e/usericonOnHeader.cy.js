a260c2f60f9dd940f3fcd0283c8dd3081739166d
HannaQA <annastelmax7@gmail.com>
2023-05-15 22:31:25 +0300

    refuctorTest

--- cypress/e2e/usericonOnHeader.cy.js
+++ cypress/e2e/usericonOnHeader.cy.js
@@ -3,35 +3,22 @@ describe('Header > User icon', ()=>{
     it('Verify User icn and clicks on drop dawn menu', () => {
         cy.get('[href="/user/admin"]').should('exist').click()
         cy.get('.icon-lg').should('exist')
-    })
-    it('check dropDown menu | builds', () => {
         cy.get('#page-header .jenkins-menu-dropdown-chevron').click({force: true})
         cy.get('#breadcrumb-menu').should('exist')
         cy.get('[href="/user/admin/builds"]').click()
         cy.get('[name= skip2content]').should('exist')
-
-    })
-    it('check dropDown menu | configure', () => {
         cy.get('#page-header .jenkins-menu-dropdown-chevron').click({force: true})
         cy.get('#breadcrumb-menu').should('exist')
         cy.get('[href="/user/admin/configure"]').click()
         cy.url().should('include', '/user/admin/configure')
-
-    })
-    it('check dropDown menu | my views', () => {
         cy.get('#page-header .jenkins-menu-dropdown-chevron').click({force: true})
         cy.get('#breadcrumb-menu').should('exist')
         cy.get('[href="/user/admin/my-views"]').click()
         cy.url().should('include', '/user/admin/my-views/view/all/')
-
-
-    })
-    it('check dropDown menu | credentials', () => {
         cy.get('#page-header .jenkins-menu-dropdown-chevron').click({force: true})
         cy.get('#breadcrumb-menu').should('exist')
         cy.get('[href="/user/admin/credentials"]').click()
         cy.url().should('include', '/user/admin/credentials/')
-
-
     })
+    
 })
