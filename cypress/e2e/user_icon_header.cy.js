4489251c5c9756a47dad257836a92531c7cc5c68
Parizat Ismailova <parizat.ismailova.kk@gmail.com>
2023-05-15 14:52:13 -0500

    refactorTest

--- cypress/e2e/user_icon_header.cy.js
+++ cypress/e2e/user_icon_header.cy.js
@@ -1,9 +1,8 @@
 /// <reference types="cypress"/>
- describe('<Header |User icon', ()=>{
+ describe('<Header | User icon', ()=>{
     it('Verify the "User" icon is clickable', ()=>{
-        cy.get('a[href="/user/peri"]').click()
+        cy.get('.model-link span[class="hidden-xs hidden-sm"').click()
         cy.url('').should('includes','/user/peri/')
-        cy.get('a[href="/user/peri"]').click()
         cy.get('.page-header .jenkins-menu-dropdown-chevron').click({force:true})
         cy.get('#yui-gen1 a[href="/user/peri/builds"]').click()
         cy.url().should('includes','/user/peri/builds')
