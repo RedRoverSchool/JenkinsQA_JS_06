7e7fd5275da232cc049a016fac5f8ed3dead855e
Larysa Reznikova <lorrez@gmail.com>
2023-05-15 14:44:52 -0500

    verification log out button3

---
+++ cypress/e2e/myViewCreateNewView.cy.js
@@ -0,0 +1,21 @@
+describe('create new view from my views', function () {
+    beforeEach('create the job', function () {
+        cy.get('a[href="newJob"]').click();
+        cy.get('#name').type('First job');
+        cy.get('.hudson_model_FreeStyleProject').click()
+        cy.get('#ok-button').click();
+        cy.get('button[name="Submit"]').click()
+        cy.get(':nth-child(1) > .model-link').click()
+    })
+
+    it('AT 09.01.002 should create new view with valid credentials', function () {
+        cy.get('a[href="/me/my-views"]').should('contain.text', 'My Views').click()
+        cy.get('a[title="New View"]').click()
+        cy.get('#name').type('first view');
+        cy.get('[for="hudson.model.MyView"]').click()
+        cy.get('#ok').click()
+        cy.get('.tab.active').should('contain', 'first view')
+        cy.get('a[href$="configure"]').click()
+    });
+
+});
