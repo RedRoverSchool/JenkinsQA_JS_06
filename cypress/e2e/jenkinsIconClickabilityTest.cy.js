32051f97ca5a552a68009325e80f4eee4245792a
Inna N. Makarova <makarovainna3@gmail.com>
2023-05-15 23:46:58 +0300

    jenkinsIconClicability(2)

--- cypress/e2e/jenkinsIconClickabilityTest.cy.js
+++ cypress/e2e/jenkinsIconClickabilityTest.cy.js
@@ -3,7 +3,10 @@
 describe('Icon', () => {
 
     it('Clicability test', () =>{
-        cy.get('#jenkins-head-icon').click()
+    cy.get('a[href="/manage"]').click()
+    cy.get('#jenkins-name-icon').click()
+    cy.get('div h1').should('be.visible')
+    cy.get('#jenkins-name-icon').should('be.visible')
 
     })
 })
