7cfc76151939b69d3747873a9f1f7d754f7683f6
Inna N. Makarova <makarovainna3@gmail.com>
2023-05-17 00:18:39 +0300

    AT_01.01_023| <Header>Head Icon clickability test

--- cypress/e2e/headerHeadIconClickabilityTest.cy.js
+++ cypress/e2e/headerHeadIconClickabilityTest.cy.js
@@ -7,5 +7,6 @@ describe('headerHeadIconClicabilityTest', () => {
     cy.get('#jenkins-name-icon').click()
     cy.get('div h1').should('be.visible')
     cy.get('#jenkins-name-icon').should('be.visible')
+    
     })
 })
