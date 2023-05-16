4b5bfcac558a723eecda030d480fb890c24bbd14
Olesia Matveevaemail <matvlesia@gmail.com>
2023-05-16 21:51:38 +0100

    add {force: true}

--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -4,7 +4,7 @@ describe('Homepage', () => {
 
   it('Verify Homepage Link "Learn more about distributed builds" is working', () => {
 
-      cy.get('.content-block__link.content-block__help-link').invoke('removeAttr', 'target').click()
+      cy.get('.content-block__link.content-block__help-link').invoke('removeAttr', 'target').click({force: true})
       cy.get('#title-text').should('contain.text', 'Jenkins : Distributed builds')
       cy.url().should('eq', 'https://wiki.jenkins.io/display/JENKINS/Distributed+builds')
   });
