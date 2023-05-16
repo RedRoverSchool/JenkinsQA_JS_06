76908934d57b9c14c6026052237d2b2eb80de193
MaryMoroz <morozova.mary@gmail.com>
2023-05-15 20:05:03 -0400

    ignore tests

--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -2,7 +2,7 @@
 
 describe('Homepage', () => {
 
-  it('Verify Homepage Link "Learn more about distributed builds" is working', () => {
+  it.skip('Verify Homepage Link "Learn more about distributed builds" is working', () => {
 
       cy.get('.content-block__link.content-block__help-link').invoke('removeAttr', 'target').click()
       cy.get('#title-text').should('contain.text', 'Jenkins : Distributed builds')
@@ -28,7 +28,7 @@ describe('Homepage', () => {
     cy.title().should('eq', 'Jenkins : Distributed builds')
   })
 
-  it ('Verify Homepage Link "Learn more about distributed builds" is working', () => {
+  it.skip('Verify Homepage Link "Learn more about distributed builds" is working', () => {
     cy.get('.content-block').contains('Learn more about distributed builds').invoke('removeAttr', 'target').click()
     cy.get("#title-text").should("contain.text", "Jenkins : Distributed builds")
     cy.url().should('eq', 'https://wiki.jenkins.io/display/JENKINS/Distributed+builds')
