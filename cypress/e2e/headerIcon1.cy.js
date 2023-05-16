035b2432c02b30c3141b59d9137b60f694a1e6b3
Kseniya <ksenia.parfenovic@gmail.com>
2023-05-16 14:45:34 +0200

    AT_01.01 _021| Verify Head Icon is clickable.

--- cypress/e2e/headerIcon1.cy.js
+++ cypress/e2e/headerIcon1.cy.js
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
 
-describe("Header | Head Icon", () => {
+describe("Header Icon 1", () => {
   it("AT_01.01 _021| Verify Head Icon is clickable.", () => {
     cy.visit("http://localhost:8080/");
     cy.get("#jenkins-head-icon").should("be.visible");
