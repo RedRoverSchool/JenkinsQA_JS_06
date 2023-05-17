bde2ba61cce7ee846f17fb911853b468cf9b9196
Nadezhda Kulikova <kulikova0101@gmail.com>
2023-05-17 23:46:37 +0200

    AT_02.05.09|ClickOnLink_LearnMoreAboutDistributedBuilds

--- cypress/e2e/distributedBuilds.cy.js
+++ cypress/e2e/distributedBuilds.cy.js
@@ -1,10 +1,16 @@
 /// <reference types="cypress"/>
 
+// const { contains } = require("cypress/types/jquery")
 
-describe('Learn more about distributed builds', () => {
+describe ('Learn more about distributed builds', () => {
     it('Verify "Learn more about distributed builds" is clickable ', () =>{
         cy.get('a[class="content-block__link content-block__help-link"]').invoke('removeAttr','target').click()
         cy.url().should('eq', 'https://wiki.jenkins.io/display/JENKINS/Distributed+builds')
         
     })
+    it ('AT_02.05.09 | Click on Link Learn more about distributed builds from Homepage', () =>{
+        cy.get('.content-block__help-link').invoke('removeAttr','target').click()
+        cy.url().should('contain', '/Distributed+builds')
+    
+})
 })
