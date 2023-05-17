19c1cbc2e3f638ead821990fedd4d55e74cc7ba7
MikeTaran <mtaran@ukr.net>
2023-05-18 01:09:33 +0300

    refact

--- cypress/e2e/headerIconVerify.cy.js
+++ cypress/e2e/headerIconVerify.cy.js
@@ -5,6 +5,7 @@ describe('Header | Head Icon', () => {
     it('Verify Jenkins icon', function () {
         cy.get('#jenkins-head-icon').should('be.visible');        
     });
+
     it('AT_01.01.032 | Header Icon verify', () => {
         cy.get('#jenkins-head-icon').should('be.visible')    
     })
