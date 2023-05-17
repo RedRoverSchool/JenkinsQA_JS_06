0b857b6801e0ad4153642d004a83bddca4d9fe09
MikeTaran <mtaran@ukr.net>
2023-05-17 12:28:55 +0300

    AT_01.01.032  Header Icon verify

--- cypress/e2e/headerIconVerify.cy.js
+++ cypress/e2e/headerIconVerify.cy.js
@@ -5,4 +5,9 @@ describe('Header | Head Icon', () => {
     it('Verify Jenkins icon', function () {
         cy.get('#jenkins-head-icon').should('be.visible');        
     });
+    //mt
+    it('AT_01.01.032 | Header Icon verify', () => {
+        cy.get('#jenkins-head-icon').should('be.visible').click()
+        cy.url().should('include', 'http://localhost:8080/')
+    })
 })
