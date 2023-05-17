ed708d5e50baee8a608b2afcb47d68f18549db4c
Mysiuk12@gmail.com <Mysiuk12@gmail.com>
2023-05-17 11:22:52 +0200

    Changed css selectors, removed { force: true }

--- cypress/e2e/buildHistory.cy.js
+++ cypress/e2e/buildHistory.cy.js
@@ -13,7 +13,7 @@ describe('Build History|View build history calendar', () => {
 
     it('AT_07.01_003|<Build History> Verify date when the build was created is visible', () => {
     
-        cy.get('.task-link-text').contains('New Item').click({ force: true });
+        cy.get('#side-panel [href$="newJob"]').click();
         cy.get('.jenkins-input').type('project1')
         cy.get("li[class='hudson_model_FreeStyleProject']").click()
         cy.get("#ok-button").click()
@@ -21,7 +21,7 @@ describe('Build History|View build history calendar', () => {
         cy.get('.model-link').contains('Dashboard').click()
         cy.get('#job_project1').should('be.visible')
         cy.get('#job_project1 > :nth-child(7)').click()
-        cy.get('.task-link-text').contains('Build History').click({ force: true });
+        cy.get('[href="/view/all/builds"').click();
         cy.get('.jenkins-app-bar__content').should('have.text', 'Build History of Jenkins');
         cy.get('.label-event-blue').click();
         cy.get('.timeline-event-bubble-time').should('be.visible')
