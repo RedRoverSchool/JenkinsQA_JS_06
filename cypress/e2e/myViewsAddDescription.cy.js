fdd5148f8ab099d07119d3576c4bb4334f1ec856
EkaterinaSokolova <sokolovaekaterina.v@gmail.com>
2023-05-17 13:55:22 +0200

    refactor test

--- cypress/e2e/myViewsAddDescription.cy.js
+++ cypress/e2e/myViewsAddDescription.cy.js
@@ -5,6 +5,7 @@ describe('<My Views > Add description',() => {
     let inputText = 'description';
 
     it ('<My Views Add description>', function() {
+        cy.contains('My Views').click();
         cy.get('#description-link').click();
         cy.get('.jenkins-input').should('be.visible').type(inputText);
         cy.get('button[name="Submit"]').click();
