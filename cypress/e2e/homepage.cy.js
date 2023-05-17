d8a429d00bfd1118c7dbedd7d68b8178249c3e7c
DimaCurkanu <kdgrig@gmail.com>
2023-05-17 16:17:40 -0500

    ignore 2 tests

--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -2,7 +2,7 @@
 
 describe('Homepage', () => {
 
-  it('Verify Homepage Link "Learn more about distributed builds" is working', () => {
+  it.skip('Verify Homepage Link "Learn more about distributed builds" is working', () => {
 
       cy.get('.content-block__link.content-block__help-link').invoke('removeAttr', 'target').click({force: true})
       cy.get('#title-text').should('contain.text', 'Jenkins : Distributed builds')
@@ -22,7 +22,7 @@ describe('Homepage', () => {
     });
   });
   
-  it('Verify Home page > Link "Learn more about distributed builds"',() => { 
+  it.skip('Verify Home page > Link "Learn more about distributed builds"',() => { 
     cy.get('a[href$="/redirect/distributed-builds"]').invoke('removeAttr', 'target').click()
     cy.url().should('include', '/display/JENKINS/Distributed+builds')
     cy.title().should('eq', 'Jenkins : Distributed builds')
