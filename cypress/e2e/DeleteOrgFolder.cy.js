e3a509287d4370031b0beaeeaf5a8adcfbcc6a5f
IrinaVorontsova1 <iraavorontsova@gmail.com>
2023-05-16 20:49:09 -0700

    AT_17.03_004 Delete Organization Folder

--- cypress/e2e/DeleteOrgFolder.cy.js
+++ cypress/e2e/DeleteOrgFolder.cy.js
@@ -45,3 +45,22 @@ describe('Organization Folder > Delete Organization Folder', () => {
 
     })
  })
+
+
+ describe('AT_17.03_004 | Organization Folder>Delete Organization Folder', () => {
+    it('create Organization Folder', () => {
+        cy.get('a[href="newJob"]').click()
+        cy.get('input[name="name"]').type('project')
+        cy.get('span.label').contains('Organization Folder').click()
+        cy.get('#ok-button').click()
+        cy.get('button[name="Submit"]').click()
+        cy.get(':nth-child(1) > .model-link').click()
+    })
+    it('delete Organization Folder', () => {
+        cy.get('tbody tr td a.jenkins-table__link').realHover()
+        cy.get('table#projectstatus button.jenkins-menu-dropdown-chevron').click()
+        cy.get('div#breadcrumb-menu ul li a').contains('Delete Organization Folder').click();
+        cy.get('button[name="Submit"]').click()
+        cy.get('.empty-state-block').should('contain', 'Welcome to Jenkins!')
+    })
+ })
