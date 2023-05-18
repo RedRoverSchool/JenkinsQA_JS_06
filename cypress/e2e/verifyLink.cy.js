1c63402f0e778df9b34ee8031adb5e017736b089
MaryMoroz <morozova.mary@gmail.com>
2023-05-18 19:23:06 -0400

    ignore tests

--- cypress/e2e/verifyLink.cy.js
+++ cypress/e2e/verifyLink.cy.js
@@ -1,12 +1,11 @@
 describe('Homepage Link Learn more about distributed builds', () => {
-
     it('Verify link Learn more about distributed builds redirects user to proper URL', () => {
         cy.get('a[href="https://www.jenkins.io/redirect/distributed-builds"]').as('linkDistrBuilds').should('be.visible')
         cy.get('@linkDistrBuilds').find('span').should('have.text', 'Learn more about distributed builds')
-
-        cy.get('@linkDistrBuilds').invoke('removeAttr', 'target').click()
         
+        cy.wait(3000)
+        cy.get('@linkDistrBuilds').invoke('removeAttr', 'target').click()
+
         cy.url().should("equal", "https://wiki.jenkins.io/display/JENKINS/Distributed+builds")
     } )
-
 })
