1f85f938552b979e4cd9e28519cfa05171357f45
Diana Taly <danchik_us@yahoo.com>
2023-05-17 19:02:22 -0700

    AT_05.08_004 | New Item link goes to New Item page

--- cypress/e2e/newItem.cy.js
+++ cypress/e2e/newItem.cy.js
@@ -1,6 +1,6 @@
 import newItemNames from '../fixtures/newItemNames.json'
 
-describe('US_05.08 New Item Names and Icons', () => {
+describe('newItem', () => {
     it('TC_05.08_007 Verify item names', () => {
         cy.get('a[href="/view/all/newJob"]').click()
         cy.get('.j-item-options li span')
@@ -19,4 +19,15 @@ describe('US_05.08 New Item Names and Icons', () => {
             })
             .should('deep.equal', newItemNames.projectNames)
     })
+
+    it('AT_05.08_004 | <New Item> New Item link goes to New Item page', () => {
+        cy.get('.task:nth-child(1) .task-icon-link').should('exist') //contains New Item icon
+        cy.get('.task:nth-child(1) a.task-link')
+            .should('exist')
+            .and('have.text', 'New Item')
+            .and('have.attr', 'href', '/view/all/newJob')
+            .click()
+        cy.get('div.add-item-name').should('exist');
+        cy.url().should('contain', '/view/all/newJob')
+    });
 })
