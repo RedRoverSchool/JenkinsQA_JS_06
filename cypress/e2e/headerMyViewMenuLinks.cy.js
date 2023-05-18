7c0a5feacc3c52d3011a683df9e945e8c2715475
Paul-Korz <paulkorzenok@gmail.com>
2023-05-18 21:43:17 +0300

    AT_01.06_004| Header | Choosing "My Views" link in user dropdown-menu

--- cypress/e2e/headerMyViewMenuLinks.cy.js
+++ cypress/e2e/headerMyViewMenuLinks.cy.js
@@ -9,4 +9,10 @@ describe("US_01.06 Header My views menu link", () => {
         cy.get('@sidePanel').find('[href="/asynchPeople/"]').should('be.visible');
         cy.get('@sidePanel').find('[href$="builds"]').should('be.visible');
     })
+
+    it('AT_01.06_004| Header | Choosing "My Views" link in user dropdown-menu', () =>{
+        cy.get('#page-header .jenkins-menu-dropdown-chevron').realHover().click()
+        cy.get('.yuimenuitemlabel').contains('My Views').click()
+        cy.url().should('includes', '/my-views/view/all/')
+    })
 });
