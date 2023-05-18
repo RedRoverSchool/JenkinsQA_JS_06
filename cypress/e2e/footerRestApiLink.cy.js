218b8473e3e42896fcf6198352e887eb83af6c05
Mysiuk12@gmail.com <Mysiuk12@gmail.com>
2023-05-18 19:17:09 +0200

    Changed test name

--- cypress/e2e/footerRestApiLink.cy.js
+++ cypress/e2e/footerRestApiLink.cy.js
@@ -6,7 +6,7 @@ describe('footerRestApiLink', () => {
         cy.get('a[href="/api/"]').should('contain.text', 'API');
     });
 
-    it('AT_03.01_007 | <Footer > Verify the clickability of a REST API button', () =>{
+    it('AT_03.01_007 | Footer | Verify the clickability of a REST API button', () =>{
         cy.get('.page-footer__links a[href="api/').click();
         cy.get('#main-panel h1').should('have.text', 'REST API');
     })
