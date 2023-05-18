2d987276d94c0b4a048c3a6a26db65fa41f78ac4
Julia-Cherniak <130078586+Julia-Cherniak@users.noreply.github.com>
2023-05-18 16:06:22 +0200

    AT_02.04.003 Check availability of side panel menu (#329)
    AT_02.04.003 Check availability of side panel menu

--- cypress/e2e/dashboardMenu.cy.js
+++ cypress/e2e/dashboardMenu.cy.js
@@ -1,5 +1,7 @@
 /// <reference types="cypress"/>
 
+import {sidePanelItems} from '../fixtures/homePage.json'
+
 describe('Homepage(Dashboard)| Side panel sub-menu', () => {
 
     it('Homepage(Dashboard) | Check quantity of items on the panel sub-menu', function () {
@@ -9,5 +11,13 @@ describe('Homepage(Dashboard)| Side panel sub-menu', () => {
     it('Homepage(Dashboard) | Verify "New Item" redirection', () => {
         cy.get('a[href="/view/all/newJob"]').click()
         cy.url().should('include', '/view/all/newJob')
+    })
+
+    it('AT_02.04.003 | Homepage(Dashboard) Check availability of side panel sub-menu with 5 items', () => {
+        cy.get('#tasks .task-link-text')
+          .should('have.length', sidePanelItems.length)
+          .each(($el, idx) => {
+            expect($el.text()).to.be.equal(sidePanelItems[idx])
+        })
     })
 })
