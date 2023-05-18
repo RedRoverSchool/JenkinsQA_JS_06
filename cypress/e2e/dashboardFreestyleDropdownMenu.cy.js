9c5a4c3f50eadd813c76d1bd455f7575886eda82
SiarheiMSV <melnichak35@gmail.com>
2023-05-17 22:02:26 -0400

    AT_20.03_002 | Verify numbers of items

--- cypress/e2e/dashboardFreestyleDropdownMenu.cy.js
+++ cypress/e2e/dashboardFreestyleDropdownMenu.cy.js
@@ -21,9 +21,9 @@ describe('dashboardFreestyleDropdownMenu', () => {
         })
     })
     
-    it.only('AT_20.03_002 | Verify numbers of items of Freestyle projects name hoverable dropdown menu', () => {
+    it.('AT_20.03_002 | Verify numbers of items of Freestyle projects name hoverable dropdown menu', () => {
         cy.get('.jenkins-table__link.model-link.inside button').click({ force: true })
         cy.get('.first-of-type li').should('have.length', freestyleDropdownItems.freestyleDropdownItems.length)
     })
-    
+
 })
