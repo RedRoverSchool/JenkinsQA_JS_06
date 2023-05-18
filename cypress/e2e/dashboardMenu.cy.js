74fe250fbb67e87e75148aa5205b20d1cacd076e
ellayezina <ellayjuly@gmail.com>
2023-05-18 11:15:50 -0400

    AT_02.04.004 | Homepage(Dashboard) | Verify "New Item" redirection

--- cypress/e2e/dashboardMenu.cy.js
+++ cypress/e2e/dashboardMenu.cy.js
@@ -5,4 +5,9 @@ describe('Homepage(Dashboard)| Side panel sub-menu', () => {
     it('Homepage(Dashboard) | Check quantity of items on the panel sub-menu', function () {
         cy.get('.task-link-wrapper ').should('have.length', 5)
     })
+
+    it('Homepage(Dashboard) | Verify "New Item" redirection', () => {
+        cy.get('a[href="/view/all/newJob"]').click()
+        cy.url().should('include', '/view/all/newJob')
+    })
 })
