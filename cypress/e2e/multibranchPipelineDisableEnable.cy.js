5e6f319cee140816903af30398c8ab7addd755a5
Milla Kaidalova <milladiz84@gmail.com>
2023-05-19 12:30:05 -0400

    fixed the selector and name

--- cypress/e2e/multibranchPipelineDisableEnable.cy.js
+++ cypress/e2e/multibranchPipelineDisableEnable.cy.js
@@ -1,15 +1,14 @@
 ///<reference types="cypress"/>
 import data from '../fixtures/multibranchPipeline.json';
 
-describe('Multibranch Pipeline - Disable/Enable Multibranch Pipeline', () => {
+describe('Multibranch Pipeline Disable/Enable', () => {
     it('AT_16.05.001 | Verify disable Multibranch Pipeline', () => {
-        cy.get( 'a[href="/view/all/newJob"]').click();
+        cy.get( 'a[href$="/newJob"]').click();
         cy.get('input.jenkins-input').type(data.newMultiPipeline);
         cy.get('.org_jenkinsci_plugins_workflow_multibranch_WorkflowMultiBranchProject').click();
         cy.get('#ok-button').click();
         cy.get('button[name="Submit"]').click()
         cy.get('button[name="Submit"]').should('includes.text', data.messageDisable).click();
-        cy.get('#enable-project').contains(data.enableMessage);
-        cy.get('button[name="Submit"]').should('includes.text', 'Enable');
+        cy.get('#enable-project').should('contain', data.enableMessage);
     });
 });
