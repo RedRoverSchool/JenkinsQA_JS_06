6919634e16afc53b89e738cf61bfd5f314d3e39c
DimaCurkanu <kdgrig@gmail.com>
2023-05-19 18:52:40 -0500

    Refactor 2 files into 1 to avoid duplication

--- cypress/e2e/freestyleProjectEditDescription.cy.js
+++ cypress/e2e/freestyleProjectEditDescription.cy.js
@@ -2,23 +2,56 @@
 import createNewView from "../fixtures/createNewView.json"
 
 describe('freestyleProjectEditDescription', () => {
+    
+    it('AT_12.07_002| Freestyle project > Add and edit description above existed project in List', function (){
+      cy.get('#description-link').click();
+      cy.get('.jenkins-input').clear().type('Description from Home page above list');
+      cy.get('button[name="Submit"]').click()
+      cy.get('#description').should('contain.text', 'Description from Home page above list')
+      cy.get('#description-link').should('contain', 'Edit description').click();
+      cy.get('.jenkins-input').type('Modify Description + ');
+      cy.get('button[name="Submit"]').click()
+     cy.get('#description').should('contain.text', 'Modify Description')
+    })
 
-  beforeEach('create Freestyle Project', function () {
-    cy.get('a[href$="my-views"]').realHover();
-    cy.get('a[href$="my-views"]').click();
-    cy.get('a[href$="/newJob"]').click();
-    cy.get('#name').type(createNewView.jobName);
-    cy.get('.hudson_model_FreeStyleProject').click();
-    cy.get('#ok-button').click();
-    cy.get('#general').should('have.text', createNewView.header);
-    cy.get('.jenkins-button--primary').click();
+    it('AT_12.07.003 | Freestyle project> Verify possibility to edit description', () => {
+      cy.get('a[href="newJob"]').click()
+      cy.get('input#name').type('Project name')
+      cy.get('.hudson_model_FreeStyleProject').click()
+      cy.get('#ok-button').click()
+      cy.get('[name="description"]').type('Description')
+      cy.get('[name="Submit"]').click()
+
+      cy.get('#description-link').click()
+      cy.get('[name="description"]').type('New ')
+      cy.get('.jenkins-button--primary ').click()
+
+      cy.get('#description div:first-child').should('have.text', 'New Description')
+    })
+
+    it('AT_12.07_004| Freestyle project > Add and edit description into project from List', function () {
+      cy.get('a[href="newJob"]').click()
+      cy.get('input#name').type('Project1')
+      cy.get('li[tabindex="0"] span').contains('Freestyle project').click()
+      cy.get('#ok-button').click()
+      cy.get(':nth-child(1) > .model-link').click()
+      cy.get('.jenkins-table__link > span').click()
+      cy.get('#description-link').click()
+      cy.get('.jenkins-input').clear().type('Description into the project');
+      cy.get('.jenkins-button.jenkins-button--primary').click()
+      cy.get('#description').should('contain.text', 'Description into the project')
+      cy.get('#description-link').should('contain', 'Edit description').click()
+      cy.get('.jenkins-input').type('Changing Description + ')
+      cy.get('.jenkins-button.jenkins-button--primary').click()
+      cy.get('#description').should('contain.text', 'Changing Description')
+    })
+
+    it('AT_12.07_001 | Freestyle project> Edit description> Verify possiblity to type the text', function () {
+      cy.get('.leading-icon').realHover();
+      cy.get('#description-link').should('be.visible').and('includes.text', createNewView.description).click();
+      cy.get('.jenkins-input').type(createNewView.typeText);
+      cy.get('.jenkins-input').should('be.visible');
+      cy.get('.jenkins-button--primary').click();
+      cy.get('#description').contains(createNewView.typeText)
+    })
   })
-  it('AT_12.07_001 | Freestyle project> Edit description> Verify Type text', function () {
-    cy.get('.leading-icon').realHover();
-    cy.get('#description-link').should('be.visible').and('includes.text', createNewView.description).click();
-    cy.get('.jenkins-input').type(createNewView.typeText);
-    cy.get('.jenkins-input').should('be.visible');
-    cy.get('.jenkins-button--primary').click();
-    cy.get('#description').contains(createNewView.typeText)
-  });
-})
