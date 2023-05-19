5604cd0c24b7caa591941c28e3a4c8d925a31cf2
Julia-Cherniak <cherniakjuliia@gmail.com>
2023-05-19 12:32:55 +0200

    Added variable and changed name of 'it'

--- cypress/e2e/createMultibranchPipeline.cy.js
+++ cypress/e2e/createMultibranchPipeline.cy.js
@@ -1,14 +1,16 @@
 describe('New Item Create a new Multibranch Pipeline', () => {
-    it('Create a new Multibranch Pipeline', () => {
+    const nameOfPipeline = 'TestMultibranchPipeline'
+    
+    it('Create a new Multibranch Pipeline using [+New Item]', () => {
         cy.get('a[href="/view/all/newJob"]').click()
-        cy.get('#name').type('TestPipeline')
+        cy.get('#name').type(nameOfPipeline)
         cy.get('#j-add-item-type-nested-projects').contains('Multibranch Pipeline').click()
         cy.get('#ok-button').click()
         cy.get('button[name="Submit"]').click()
-        cy.get('#main-panel').should('contain', 'TestPipeline')
+        cy.get('#main-panel').should('contain', nameOfPipeline)
         cy.get('#breadcrumbBar li:nth-child(1)').click()
         cy.get('#projectstatus')
             .should('exist')
-            .and('include.text', 'TestPipeline')
+            .and('include.text', nameOfPipeline)
     })
 })
