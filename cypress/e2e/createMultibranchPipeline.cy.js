a01d6fd4c8783f1d035552073e8c9dd744a628f5
Julia-Cherniak <cherniakjuliia@gmail.com>
2023-05-19 15:09:31 +0200

    add fixtures

--- cypress/e2e/createMultibranchPipeline.cy.js
+++ cypress/e2e/createMultibranchPipeline.cy.js
@@ -1,16 +1,17 @@
+import pipelineName from '../fixtures/pipelineName.json'
+
 describe('New Item Create a new Multibranch Pipeline', () => {
-    const nameOfPipeline = 'TestMultibranchPipeline'
 
     it('AT_05.05_004 Create a new Multibranch Pipeline using [+New Item]', () => {
         cy.get('a[href="/view/all/newJob"]').click()
-        cy.get('#name').type(nameOfPipeline)
+        cy.get('#name').type(pipelineName.namePipeline)
         cy.get('#j-add-item-type-nested-projects').contains('Multibranch Pipeline').click()
         cy.get('#ok-button').click()
         cy.get('button[name="Submit"]').click()
-        cy.get('#main-panel').should('contain', nameOfPipeline)
+        cy.get('#main-panel').should('contain', pipelineName.namePipeline)
         cy.get('#breadcrumbBar li:nth-child(1)').click()
         cy.get('#projectstatus')
             .should('exist')
-            .and('include.text', nameOfPipeline)
+            .and('include.text', pipelineName.namePipeline)
     })
 })
