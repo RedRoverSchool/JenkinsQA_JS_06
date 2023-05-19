6c99572fa626ecdfb591d93be1da3d3bf51b2614
MaryMoroz <morozova.mary@gmail.com>
2023-05-19 10:16:28 -0400

    [ERR] verifyLink | Verify link Learn more

--- cypress/e2e/verifyLink.cy.js
+++ cypress/e2e/verifyLink.cy.js
@@ -1,11 +1,15 @@
-describe.skip('Homepage Link Learn more about distributed builds', () => {
-    it('Verify link Learn more about distributed builds redirects user to proper URL', () => {
+describe('verifyLink', () => {
+    it('AT_02.05_005 | Verify link Learn more about distributed builds redirects user to proper URL', () => {
+        cy.intercept('/redirect/distributed-builds').as('redirect')
+        cy.intercept('/display/JENKINS/Distributed+builds').as('newUrl')
+        
         cy.get('a[href="https://www.jenkins.io/redirect/distributed-builds"]').as('linkDistrBuilds').should('be.visible')
         cy.get('@linkDistrBuilds').find('span').should('have.text', 'Learn more about distributed builds')
         
-        cy.wait(3000)
         cy.get('@linkDistrBuilds').invoke('removeAttr', 'target').click()
-
-        cy.url().should("equal", "https://wiki.jenkins.io/display/JENKINS/Distributed+builds")
+        cy.wait('@redirect')
+        cy.wait('@newUrl')
+        
+        cy.url().should("eq", "https://wiki.jenkins.io/display/JENKINS/Distributed+builds")
     } )
 })
