7942cdc9036fb00f27a011bba7aa72578ddb9e8b
Milla Kaidalova <milladiz84@gmail.com>
2023-05-19 11:08:07 -0400

    fix the selector

--- cypress/e2e/verifyDisableMultibranchPipeline.cy.js
+++ cypress/e2e/verifyDisableMultibranchPipeline.cy.js
@@ -3,7 +3,7 @@ import data from '../fixtures/multibranchPipeline.json';
 
 describe('Multibranch Pipeline - Disable/Enable Multibranch Pipeline', () => {
     it('AT_16.05.001 | Verify disable Multibranch Pipeline', () => {
-        cy.get('.task:nth-child(1)').contains('New Item').click();
+        cy.get( 'a[href="/view/all/newJob"]').click();
         cy.get('input.jenkins-input').type(data.newMultiPipeline);
         cy.get('.org_jenkinsci_plugins_workflow_multibranch_WorkflowMultiBranchProject').click();
         cy.get('#ok-button').click();
