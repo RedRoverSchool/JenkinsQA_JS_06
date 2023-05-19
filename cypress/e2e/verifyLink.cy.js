deae725f9da480f4ae5c70b0ebfe9e57175da5d5
MaryMoroz <morozova.mary@gmail.com>
2023-05-19 12:41:18 -0400

    refactor

--- cypress/e2e/verifyLink.cy.js
+++ cypress/e2e/verifyLink.cy.js
@@ -3,6 +3,7 @@ describe('verifyLink', () => {
         cy.get('a[href="https://www.jenkins.io/redirect/distributed-builds"]').as('linkDistrBuilds').should('be.visible')
         cy.get('@linkDistrBuilds').find('span').should('have.text', 'Learn more about distributed builds')
         cy.clearAllSessionStorage()
+        
         cy.get('@linkDistrBuilds').invoke('removeAttr', 'target').click()
 
         cy.url().should("eq", "https://wiki.jenkins.io/display/JENKINS/Distributed+builds")
