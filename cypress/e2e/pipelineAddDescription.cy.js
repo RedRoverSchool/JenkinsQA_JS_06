fd82b576772d4df8284220f1edca912b1beb089a
Anna <aiurchyk@gmail.com>
2023-05-19 15:56:04 -0400

    delete my test from pipelineAddDescription

--- cypress/e2e/pipelineAddDescription.cy.js
+++ cypress/e2e/pipelineAddDescription.cy.js
@@ -20,19 +20,4 @@ describe('Add description to the pipeline', () =>{
 
     })
 
-    it.only('Verify User is able to go to tab “Edit description”', () => {
-        cy.get('#description-link').click()
-        cy.get('.jenkins-input   ').type('First Description')
-        cy.get('.jenkins-button--primary ').click();
-        cy.get('#description-link').should('contain','Edit description').click()
-        cy.get('textarea[name="description"]').should('have.value', 'First Description');
-        //cy.get('#description-link').contains('Edit description')
-        //cy.contains('#description-link', 'Edit Description').should('exist')
-        // cy.get('#description-link').then($element => {
-        //     const text = Cypress.$($element).css('content');
-        //     const extractedText = text.replace(/["']/g, ''); // Remove the quotes surrounding the text
-        //     expect(extractedText).to.equal('Edit Description');
-        // });
-    });
-
 })
