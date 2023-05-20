27f8131772a86e5f16e1b8bd661d280c65858f74
mborovaya <borovaia.marina@gmail.com>
2023-05-19 18:08:09 -0700

    Fixes from review

--- cypress/e2e/homepageDescription.cy.js
+++ cypress/e2e/homepageDescription.cy.js
@@ -1,18 +1,15 @@
 /// <reference types="cypress"/>
+import * as descriptions from '../fixtures/descriptionsProject.json';
 
-describe('<Homepage (Dashboard) > Main panel description',()=>{
-    const addDescription = 'Add description'
-    const messageInDescription = 'This is a description'
-    const editDescription = 'Edit description'
+describe('homepageDescription',()=>{
 
     it('AT_02.06_11 | Homepage (Dashboard) > Adding Main panel description', () => {
-        cy.get('#description-link').should('contain', addDescription).click();
+        cy.get('#description-link').should('contain', descriptions.addDescriptionButtonText).click();
         cy.get('#description-link').should('not.exist');
-        cy.get('[name="description"]').type(messageInDescription);
-        cy.get('.jenkins-button[formnovalidate="formNoValidate"]').should('have.text', 'Save').click();
-        cy.get('#description-link').should('contain', editDescription);
-        cy.get('#description').should('contain', messageInDescription);
+        cy.get('[name="description"]').type(descriptions.addDescriptionProject);
+        cy.get('.jenkins-button[formnovalidate="formNoValidate"]').should('have.text', descriptions.saveButtonText).click();
+        cy.get('#description-link').should('contain', descriptions.editDescriptionButtonText);
+        cy.get('#description').should('contain', descriptions.addDescriptionProject);
       })
     
 })
-
