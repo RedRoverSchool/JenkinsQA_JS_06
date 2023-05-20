3f821a87ef37a24d9d1a9b935a6330df1597d1dd
Happy Strawberry <irliatuz@gmail.com>
2023-05-19 21:15:51 -0500

    changes added

--- cypress/e2e/myViewDeleteView.cy.js
+++ cypress/e2e/myViewDeleteView.cy.js
@@ -1,24 +1,26 @@
 /// <reference types="cypress"/>
 
+const myViewName = 'First Job'
+const myView = 'MyView'
+const deleteView = 'Delete View'
 
 describe('myViewsDeleteView', () => {
-    it('TC_09.06.001 | <My views> Delete View', function () {
-        cy.get('#tasks > div:nth-child(5) > span > a > span.task-link-text').realHover();
-        cy.get('#tasks > div:nth-child(5) > span > a > span.task-link-text').click({ force: true });
+    it('AT_09.06.001 | <My views> Delete View', function () {
+        cy.get('#tasks > div:nth-child(5) > span > a > span.task-link-text').realHover().click({ force: true });
         cy.get('a[href$="/newJob"]').click();
-        cy.get('#name').type('First Job');
+        cy.get('#name').type(myViewName);
         cy.get('.hudson_model_FreeStyleProject').click();
         cy.get('#ok-button').click();
         cy.get('.jenkins-button--primary').click();
         cy.get(':nth-child(1) > .model-link').click();
         cy.get('.addTab').click();
-        cy.get('#name').type('MyView');
+        cy.get('#name').type(myView);
         cy.get('label[for="hudson.model.MyView"]').click();
         cy.get('#ok').click();
         cy.get('.active > a').click();
-        cy.get('.task-link-text').contains('Delete View').click({ force: true });
+        cy.get('.task-link-text').contains(deleteView).click({ force: true });
         cy.get('#main-panel > form > button').click();
-        cy.get('.addTab').contains('MyView').should('not.exist');
+        cy.get('.addTab').contains(myView).should('not.exist');
     });
 })
 
