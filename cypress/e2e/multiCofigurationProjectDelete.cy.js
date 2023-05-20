4eee3b9792efedaba0f439484a69898a87da3655
Alex Kushnir <classic.ua@gmail.com>
2023-05-20 11:49:17 -0700

    review accept

--- cypress/e2e/multiCofigurationProjectDelete.cy.js
+++ cypress/e2e/multiCofigurationProjectDelete.cy.js
@@ -1,6 +1,6 @@
-import rightSideMenu from '../fixtures/multiConfigurationProjectSideMenu.json'
+import {sideMenuItems, dropdownItems} from '../fixtures/multiConfigurationProject.json'
 import projects from '../fixtures/projects.json'
-import multiConfigurationDropdownMenu from '../fixtures/multiConfigurationProjectDropdownMenu.json'
+
 
 const PORT = Cypress.env("local.port");
 
@@ -17,14 +17,14 @@ describe('multiConfigurationProjectDelete', () => {
 
     it('AT_14.07_001 | Multi Configuration Project Delete | Delete Multi-configuration project within itself', () => {
         cy.get(`a[href='job/${projects.multiConfigurationProject.name}/']`).click();
-        cy.get(".confirmation-link").contains(`${rightSideMenu.MCPSideMenuItems[5]}`).click();
+        cy.get(".confirmation-link").contains(`${sideMenuItems[5]}`).click();
         cy.get("#page-body").should("not.have.text", `${projects.multiConfigurationProject.name}`);
     });
 
     it('AT_14.07_002 |Multi-configuration project| Delete Multi-configuration project with dropdown menu', () => {
         cy.get(".jenkins-table__link .jenkins-menu-dropdown-chevron").realHover().click();
         cy.get("li:nth-of-type(5)  span")
-        .contains(`${multiConfigurationDropdownMenu.multiConfigurationDropdownItems[4]}`)
+        .contains(`${dropdownItems[4]}`)
         .click();
         cy.url().should('equal', `http://localhost:${PORT}/`);
         cy.get("#page-body").should("not.have.text", `${projects.multiConfigurationProject.name}`);
