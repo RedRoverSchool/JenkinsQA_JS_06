d782e6047f5c22eb194f503b7b64d1d234f8a09b
Katerina <katerina@a0a0.ru>
2023-05-21 21:38:13 +0300

    test was added to the existing one

--- cypress/e2e/renameFreestyleProjectValid.cy.js
+++ cypress/e2e/renameFreestyleProjectValid.cy.js
@@ -1,33 +1,42 @@
 /// <reference types="cypress"/>
 
-import userD from "../fixtures/renameProjectValid.json"
+import userData from "../fixtures/renameProjectValid.json"
 
-describe('renameFreestyleProjectValid', () => {
-    
-    beforeEach ('Prepare freeStyle Project', function () {
-      cy.get(".task-link").eq(0).click();
-      cy.get("#name").type(userD.projectName);
-      cy.get('.hudson_model_FreeStyleProject').click();
-      cy.get('#ok-button').should('have.text', userD.ok).click();
-      cy.get('.setting-main  textarea  ').eq(0).type(userD.text);
-      cy.get('#bottom-sticker > div > button.jenkins-button.jenkins-button--primary')
-        .click();
+describe('FreestyleProjectRenameFreestyleProject', () => {
 
-      cy.get('.jenkins-breadcrumbs__list-item > .model-link').eq(0).click();
+  beforeEach('Prepare freeStyle Project', function () {
+    cy.get(".task-link").eq(0).click();
+    cy.get("#name").type(userData.projectName);
+    cy.get('.hudson_model_FreeStyleProject').click();
+    cy.get('#ok-button').should('have.text', userData.ok).click();
+    cy.get('.setting-main  textarea  ').eq(0).type(userData.text);
+    cy.get('#bottom-sticker > div > button.jenkins-button.jenkins-button--primary')
+      .click();
 
-      cy.get('td> a > span').should('have.text', userD.projectName).realHover();
-      cy.get('td> a > button').should('be.visible').click();
-      cy.get('#breadcrumb-menu>div.bd>ul.first-of-type>li>a>span')
-        .should('be.visible')
-        .and('have.length', 6)
-        .eq(5)
-        .click();
+    cy.get('.jenkins-breadcrumbs__list-item > .model-link').eq(0).click();
+
+    cy.get('td> a > span').should('have.text', userData.projectName).realHover();
+    cy.get('td> a > button').should('be.visible').click();
+    cy.get('#breadcrumb-menu>div.bd>ul.first-of-type>li>a>span')
+      .should('be.visible')
+      .and('have.length', 6)
+      .eq(5)
+      .click();
   });
-      it("AT_12.03.012 Rename project, new name is valid", () => {
-        cy.get('input[name="newName"]').clear().type(userD.newProjectName);
-        cy.get('button[name="Submit"]').click();
+  it("AT_12.03.004 Rename project, new name is valid", () => {
+    cy.get('input[name="newName"]').clear().type(userData.newProjectName);
+    cy.get('button[name="Submit"]').click();
 
-        cy.get('h1.job-index-headline.page-headline').should('have.text', `Project ${userD.newProjectName}`);         
-    });
+    cy.get('h1.job-index-headline.page-headline').should('have.text', `Project ${userData.newProjectName}`);
+  });
+
+  it("AT_12.03.005 Rename project, new name isn't valid", () => {
+    cy.get('input[name="newName"]').clear().type(userData.projectName);
+    cy.get('button[name="Submit"]').click();
+
+    cy.get('div h1').contains(userData.error);
+    cy.get('div#main-panel p').should('have.text', userData.errorDescription);
+
+  });
 
 });
