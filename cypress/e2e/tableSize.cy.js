55cf47e93d87d3ba743e4c55ef34a0e44871e123
Volkova Maria <MaRi221994@yandex.ru>
2023-05-21 20:48:45 +0700

    AT_20.01_002 | Dashboard> Icons M

--- cypress/e2e/tableSize.cy.js
+++ cypress/e2e/tableSize.cy.js
@@ -16,4 +16,21 @@ describe ('Dashboard Icons S,M,L', ()=> {
             })
         })
     })
+    it ('20.01_002_DashboardIcons_M', ()=> {
+        cy.get('a[href="newJob"]').click()
+        cy.get('input#name').type('Project1')
+        cy.get('li[tabindex="0"] span').contains('Freestyle project').click()
+        cy.get('#ok-button').click()
+        cy.get(':nth-child(1) > .model-link').click()
+        
+        cy.get('a[href="/iconSize?24x24"]').contains('M').click()
+
+        cy.get('#projectstatus').then((obj) => {
+            cy.document().then(() => {
+                cy.wrap(obj).then($el => window.getComputedStyle($el[0]).getPropertyValue('--table-padding'))
+                .should('eq', '0.4rem')
+            })
+        })
+    })
+    
 })
