6a113fdc9db11d519c615d3c3051415ee5323bab
julian-ole <yula22trem@yahoo.com>
2023-05-22 14:27:43 +0800

    modified

--- cypress/e2e/homepageCreateAJobLink.cy.js
+++ cypress/e2e/homepageCreateAJobLink.cy.js
@@ -1,14 +1,15 @@
 /// <reference types="cypress"/>
 
-import homepageMainPanel from /fixtures/homepageMainPanel
+import homepageMainPanel from "../fixtures/homepageMainPanel";
 
 describe("Homepage > 'Create a job' link", () => {
   it('AT_02.07.004 | <Main Panel> Verify the "Create a job" link in the "Start building your software project" section', function () {
-    cy.get('.content-block a[href="newJob"]')
-      .should("be.visible")
-      .should("have.text", homepageMainPanel.linkName)
-      .click();
+    cy.get('.content-block a[href="newJob"]').should("be.visible");
+    and("have.text", homepageMainPanel.linkName).click();
     cy.url().should("include", homepageMainPanel.createAJobEndPointUrl);
-    cy.get(".add-item-name .h3").should("have.text", homepageMainPanel.addItemLabelName);
+    cy.get(".add-item-name .h3").should(
+      "have.text",
+      homepageMainPanel.addItemLabelName
+    );
   });
 });
