eefe244b3970d18ae97d451e5e084f135a07167d
julian-ole <yula22trem@yahoo.com>
2023-05-22 16:11:04 +0800

    Assertions are edited

--- cypress/e2e/homepageCreateAJobLink.cy.js
+++ cypress/e2e/homepageCreateAJobLink.cy.js
@@ -2,10 +2,12 @@
 
 import homepageMainPanel from "../fixtures/homepageMainPanel";
 
-describe("Homepage > 'Create a job' link", () => {
+describe("Homepage, Create a job Link", () => {
   it('AT_02.07.004 | <Main Panel> Verify the "Create a job" link in the "Start building your software project" section', function () {
-    cy.get('.content-block a[href="newJob"]').should("be.visible");
-    and("have.text", homepageMainPanel.linkName).click();
+    cy.get('.content-block a[href="newJob"]')
+      .should("be.visible")
+      .and("have.text", homepageMainPanel.linkName)
+      .click();
     cy.url().should("include", homepageMainPanel.createAJobEndPointUrl);
     cy.get(".add-item-name .h3").should(
       "have.text",
