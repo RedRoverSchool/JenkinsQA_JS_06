5f694bda8d0359508951cd86be1df49c12388a15
julian-ole <yula22trem@yahoo.com>
2023-05-22 14:19:06 +0800

    fixtures are added, describe is edited

--- cypress/e2e/homepageCreateAJobLink.cy.js
+++ cypress/e2e/homepageCreateAJobLink.cy.js
@@ -1,12 +1,14 @@
 /// <reference types="cypress"/>
 
-describe("Homepage > Main Panel > 'Create a job' link", () => {
+import homepageMainPanel from /fixtures/homepageMainPanel
+
+describe("Homepage > 'Create a job' link", () => {
   it('AT_02.07.004 | <Main Panel> Verify the "Create a job" link in the "Start building your software project" section', function () {
     cy.get('.content-block a[href="newJob"]')
       .should("be.visible")
-      .should("have.text", "Create a job")
+      .should("have.text", homepageMainPanel.linkName)
       .click();
-    cy.url().should("include", "/newJob");
-    cy.get(".add-item-name .h3").should("have.text", "Enter an item name");
+    cy.url().should("include", homepageMainPanel.createAJobEndPointUrl);
+    cy.get(".add-item-name .h3").should("have.text", homepageMainPanel.addItemLabelName);
   });
 });
