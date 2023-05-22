d76f9127b12b639a2a466c54354706db34d8860c
TatiChap <tatsiana.chapurkina@gmail.com>
2023-05-21 21:02:51 -0700

    AT_18.01_002 | Profile Page | Verify Profile Icon in Header

--- cypress/e2e/profilePage.cy.js
+++ cypress/e2e/profilePage.cy.js
@@ -28,4 +28,9 @@ describe("Profile Page", () => {
       });
     });
   });
+
+  it("AT_18.01_002 | Profile Page | Verify Profile Icon in Header", function () {
+    cy.get("div[class*='login page-header']>a[href^='/user']").click();
+    cy.get('a[href^="/user"] > svg').should("exist");
+  });
 });
