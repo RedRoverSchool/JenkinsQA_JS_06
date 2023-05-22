91ad08f97c2339c7b71e615389d899a620bd5e96
Inna N. Makarova <makarovainna3@gmail.com>
2023-05-22 10:22:01 +0300

    AT_01.04.08|Header User Builds link examination

--- cypress/e2e/headerUserBuildslinkExamination.cy.js
+++ cypress/e2e/headerUserBuildslinkExamination.cy.js
@@ -2,7 +2,7 @@
 describe('headerUserBuildslinkExamination ', () =>{
     it('AT_01.04.08|Header User Builds link examination', ()=> {
        cy.get('.page-header a .jenkins-menu-dropdown-chevron').realHover().click()
-       cy.get(`a[hre$="/builds"]`).click()
+       cy.get(`a[href="/user/${Cypress.env('local.admin.username')}/builds"]`).click()
        cy.url().should('eq', `http://localhost:${Cypress.env('local.port')}/user/${Cypress.env('local.admin.username')}/builds`) 
        cy.get('#projectStatus').should('be.visible')
        cy.get('#side-panel').should('be.visible')
