83bbf7c47785ebe0dd5640d3f6d376da77210ffc
Helen.Nosenko <lina.nosenko@gmail.com>
2023-05-22 16:57:23 +0300

    AT_01.08_028

--- cypress/e2e/logOutButtonVerifying.cy.js
+++ cypress/e2e/logOutButtonVerifying.cy.js
@@ -4,5 +4,10 @@ describe('Header Log out button', () => {
   it('Verify logout button is clickable and redirects to login page', function () {
     cy.get('a[href="/logout"]').should('be.visible').and('have.text','log out').click();
     cy.url().should('contain', 'login');    
-  });      
+  }); 
+  
+  it('verify loG out button', function (){
+    cy.get('body > header:nth-child(2) > div:nth-child(3) > a:nth-child(4) > span:nth-child(2)').click()
+   })
+
 });
