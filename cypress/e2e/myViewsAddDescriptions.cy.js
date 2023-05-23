ee978a1435fad8b828bf8631b718c09e62aed261
EkaterinaSokolova <sokolovaekaterina.v@gmail.com>
2023-05-23 21:19:00 +0200

    fixures added

--- cypress/e2e/myViewsAddDescriptions.cy.js
+++ cypress/e2e/myViewsAddDescriptions.cy.js
@@ -1,4 +1,5 @@
 /// <reference types="cypress"/>
+import descriptionsProject from '../fixtures/descriptionsProject.json'
 
 describe('My Views add description', () => {
 
@@ -10,13 +11,16 @@ describe('My Views add description', () => {
         cy.get('#description div:nth-child(1)').should('have.text','DESCRIPTION')
     })
 
-    let inputText = 'description';
     it('AT 09.02.005| My Views> Add description', function () {
         cy.contains('My Views').click();
         cy.get('#description-link').click();
-        cy.get('.jenkins-input').should('be.visible').clear().type(inputText);
+        cy.get('.jenkins-input')
+            .should('be.visible')
+            .clear()
+            .type(descriptionsProject.addDescriptionProject);
         cy.get('button[name="Submit"]').click();
-        cy.get('#description>div:nth-child(1)').should('have.text', inputText);
+        cy.get('#description>div:nth-child(1)')
+            .should('have.text', descriptionsProject.addDescriptionProject);
     });
 
 })
