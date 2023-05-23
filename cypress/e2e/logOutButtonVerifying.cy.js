52544e66c0e06b8672a3329f67a5a211ea396fbb
lina2989 <lina.nosenko@gmail.com>
2023-05-23 15:38:14 +0300

    added new title verification

--- cypress/e2e/logOutButtonVerifying.cy.js
+++ cypress/e2e/logOutButtonVerifying.cy.js
@@ -1,4 +1,5 @@
 /// <reference types="cypress"/>
+import loginPage from '../fixtures/logInPage.json';
 
 describe('Header Log out button', () => {
   it('Verify logout button is clickable and redirects to login page', function () {
@@ -10,4 +11,9 @@ describe('Header Log out button', () => {
     cy.get('body > header:nth-child(2) > div:nth-child(3) > a:nth-child(4) > span:nth-child(2)').click()
    })
 
+   it('verify title page after log out', function(){
+    cy.get('a[href="/logout"] span.hidden-xs').click()
+    cy.title().should('eq', loginPage.loginMessage)
+   })
+   
 });
