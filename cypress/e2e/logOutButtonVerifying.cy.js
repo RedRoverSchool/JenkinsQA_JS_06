078b5bd5975277c86365f68a5c879b5a867ec594
lina2989 <lina.nosenko@gmail.com>
2023-05-23 14:34:10 +0300

    updated

--- cypress/e2e/logOutButtonVerifying.cy.js
+++ cypress/e2e/logOutButtonVerifying.cy.js
@@ -9,5 +9,5 @@ describe('Header Log out button', () => {
   it('verify loG out button', function (){
     cy.get('body > header:nth-child(2) > div:nth-child(3) > a:nth-child(4) > span:nth-child(2)').click()
    })
-
+// mama
 });
