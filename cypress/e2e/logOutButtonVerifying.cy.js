abefc08db7e273172c4330707da6b405498886d2
lina2989 <lina.nosenko@gmail.com>
2023-05-24 11:31:44 +0300

    RF2

--- cypress/e2e/logOutButtonVerifying.cy.js
+++ cypress/e2e/logOutButtonVerifying.cy.js
@@ -3,7 +3,7 @@ import loginPage from '../fixtures/logInPage.json';
 
 describe('Header Log out button', () => {
    
-  it('verify loG out button', function (){
+  it('AT_01.08_028', function (){
     cy.get('body > header:nth-child(2) > div:nth-child(3) > a:nth-child(4) > span:nth-child(2)').click()
    })
      
