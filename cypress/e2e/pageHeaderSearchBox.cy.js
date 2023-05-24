10cc0054651462cf9a17c9fc3db1a59c1e02a284
Olha Dymaretska <olhadymaretska@gmail.com>
2023-05-24 01:03:38 -0400

    3

--- cypress/e2e/pageHeaderSearchBox.cy.js
+++ cypress/e2e/pageHeaderSearchBox.cy.js
@@ -1,7 +1,6 @@
 /// <reference types="cypress"/>
 
 describe('pageHeaderSearchBox', () => {
-describe('pageHeaderSearchBox', () => {
     it('AT_01.02_005 verify Search Box is visible on the home page', function () {
         cy.get('#searchform').should('be.visible').click().type('project')
         cy.get('#search-box-sizer').should('have.text', 'project')
@@ -15,9 +14,5 @@ describe('pageHeaderSearchBox', () => {
     it('AT_01.02_010 verify Search Box is visible on the People page', function () {
         cy.get('.task  a[href="/asynchPeople/"]').click()
         cy.get('#searchform').should('be.visible')
-    });
-    it('AT_01.02_009 verify Search Box is visible on the New Item page', function () {
-        cy.get('.task  a[href="/view/all/newJob"]').click()
-        cy.get('#searchform').should('be.visible').click().type('project') 
     });
 });
