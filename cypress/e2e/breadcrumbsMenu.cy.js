9b20b565cfdfcb6a4fd3c7dcb790f2df0bc1fb6d
Alex Kushnir <classic.ua@gmail.com>
2023-05-24 10:22:39 -0700

    [ERR] breadcrumbsMenu

--- cypress/e2e/breadcrumbsMenu.cy.js
+++ cypress/e2e/breadcrumbsMenu.cy.js
@@ -66,7 +66,7 @@ describe('BreadcrumbsMenu', () => {
 
    it('AT_04.02.009 |Breadcrumbs| Dashboard page link check Manage Jenkins dropdown subfolder', () => {
       cy.get('#breadcrumbs .jenkins-menu-dropdown-chevron').realHover().realClick();
-      cy.get('#breadcrumb-menu .first-of-type:nth-of-type(1)').contains(homePage.dashboardDropdownItems[3]).trigger('mouseover');
+      cy.get("#breadcrumb-menu a[href$='manage'] span").contains(homePage.dashboardDropdownItems[3]).trigger('mouseover');
       homePage.manageJenkinsDropdownItems.forEach(item => {
          cy.contains('#submenu0', item).should('be.visible');
        });
