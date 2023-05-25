030f5a51ed6873c62f0666057089160154d54c0b
Alxndrpz <sashapozniak2@gmail.com>
2023-05-25 16:49:33 +0300

    AT_01.01.45 | <Header> Head Icon is visible

--- cypress/e2e/headerHeadIcon.cy.js
+++ cypress/e2e/headerHeadIcon.cy.js
@@ -90,6 +90,10 @@ describe('Header Head Icon', () => {
         cy.get('.empty-state-block > h1').should('contain', logInPage.loginPageHeader)
     })
 
-
+    it('AT_01.01.45 | <Header> Head Icon is visible and redirects to home page', () => {
+        cy.get('a[href="/view/all/newJob"]').click()
+        cy.get('#jenkins-home-link').should('be.visible').click();
+        cy.get('h1').should('have.text', 'Welcome to Jenkins!').and('be.visible');
+    })
 })
 
