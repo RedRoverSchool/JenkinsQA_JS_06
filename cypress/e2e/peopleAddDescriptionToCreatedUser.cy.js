8d1b838372e4b1cbaafeb54f507e458f61d129ea
Klavdia <kgold94@yahoo.com>
2023-05-26 13:12:48 -0700

    fixture created for test

--- cypress/e2e/peopleAddDescriptionToCreatedUser.cy.js
+++ cypress/e2e/peopleAddDescriptionToCreatedUser.cy.js
@@ -1,16 +1,18 @@
 /// <reference types='cypress'/>
 
-describe("Add description to a created user", () => {
+import data from "../fixtures/peopleAddDescription.json"
+
+describe("peopleAddDescriptionToCreatedUser", () => {
   beforeEach("Create a user from Manage Jenkins>Manage Users", function () {
     cy.get('a[href="/manage"]').click();
     cy.get('.jenkins-section__item a[href="securityRealm/"]').click();
     cy.get(".jenkins-app-bar__controls").click();
-    cy.get("input#username").type("created1");
-    cy.get('input[name="password1"]').type("123");
-    cy.get('input[name="password2"]').type("123");
-    cy.get('input[name="email"]').type("qa@test.com");
+    cy.get("input#username").type(data.userName);
+    cy.get('input[name="password1"]').type(data.password);
+    cy.get('input[name="password2"]').type(data.confirmPassword);
+    cy.get('input[name="email"]').type(data.emailAddress);
     cy.get('button[name="Submit"]').click();
-    cy.get("#breadcrumbBar").contains("Dashboard").click();
+    cy.get("#breadcrumbBar").contains(data.dashboard).click();
   });
 
   it("AT_06.02.004 verify the description can be added to a created user", () => {
@@ -18,13 +20,11 @@ describe("Add description to a created user", () => {
     cy.get('#person-created1 a[href = "/user/created1/"]').click();
     cy.get("#description div").not("div[class]").should("be.empty");
     cy.get("#description-link").click();
-    cy.get("textarea[name='description']").type(
-      "Created first time a new description"
-    );
+    cy.get("textarea[name='description']").type(data.description);
     cy.get("button[name='Submit']").click();
     cy.get("#description div")
       .not("div[class]")
-      .should("have.text", "Created first time a new description");
+      .should("have.text", data.description);
   });
 
   afterEach("Delete created user", function () {
