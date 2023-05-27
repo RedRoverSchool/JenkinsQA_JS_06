1ecd1c903866c86d09517b82049fe5628dfb6d9b
olgalex <alex_olg@ukr.net>
2023-05-27 21:59:24 +0300

    RF

--- cypress/e2e/createJob.cy.js
+++ cypress/e2e/createJob.cy.js
@@ -9,7 +9,7 @@ describe('AT_02.01_002 | Homepage Create a clickable job link', () => {
         cy.get('#ok-button').click()
         cy.get('#general').should('be.visible')
     });
-    it ('AT_05.04.03 | New item Folder', () => {
+    it ('AT_05.04.03 | <New item> User is able to create Folder project', () => {
         cy.get("a[href='/view/all/newJob']").should('have.text', 'New Item').click()
         cy.url().should("eq", 'http://localhost:8080/view/all/newJob')
         cy.get('#name').type('olgalex').should('be.visible')
