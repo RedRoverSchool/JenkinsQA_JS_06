560222c7058e55df6d552463bb8ca3251974cfed
Parizat Ismailova <parizat.ismailova.kk@gmail.com>
2023-05-29 21:48:49 -0500

    refactorTest

--- cypress/e2e/editExistingDescription.cy.js
+++ cypress/e2e/editExistingDescription.cy.js
@@ -22,7 +22,7 @@ describe(`Edit description`, function () {
   it('AT_20.02_ 002| Dashboard | Verify the "Edit Description" link', ()=>{
       cy.get('#description-link').click()
       cy.get('.jenkins-input   ').type(descriptionsProject.addDescriptionProject)
-      cy.get('button[class="jenkins-button jenkins-button--primary "]').click() 
+      cy.get('.jenkins-button').click() 
       cy.should('exist',descriptionsProject.addDescriptionProject)
       cy.get('#description-link').click()
       cy.get('textarea[name="description"]').type('{selectall}{backspace}').type(descriptionsProject.addNewDescriptionProject)
