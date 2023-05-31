89f0d655acf55b8cd831667fe9ecccc3c01c3835
shablouskaya <shablouskaya.o@gmail.com>
2023-05-31 14:15:42 -0400

    AT_18.04_002 | <Profile Page> | Link to User's configure | Configure is displayed on User's configure profile page

--- cypress/e2e/profilePageConfigureLink.cy.js
+++ cypress/e2e/profilePageConfigureLink.cy.js
@@ -9,4 +9,11 @@ describe ('profilePageConfigureLink', () => {
         cy.get(`a[href="/user/${userName}/configure"]`).should("be.visible")
         cy.get(`a[href="/user/${userName}/configure"] .task-link-text`).should("have.text", "Configure")
     })
+
+    
+    it("AT_18.04_002 | <Profile Page> | Link to User's configure | Configure is displayed on User's configure profile page", () => {
+        cy.get('#page-header .jenkins-menu-dropdown-chevron').realHover().click()
+        cy.get(`a[href="/user/${userName}/configure"]`).should("be.visible").click()       
+        cy.get(`a[href="/user/${userName}/configure"] .task-link-text`).should("have.text", "Configure")
+    })
 })
