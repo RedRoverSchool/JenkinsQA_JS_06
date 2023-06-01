c013a648333876b8696bfd0db69650e7ed022454
Assem Saginayeva <asemsaginayeva@gmail.com>
2023-06-01 14:27:06 +0200

    AT_05.08_015 | <New Item> Verify Items

--- cypress/e2e/newItem.cy.js
+++ cypress/e2e/newItem.cy.js
@@ -41,21 +41,21 @@ describe('newItem', () => {
     it('AT_05.08_009 | New item | Verify that page with New Items displayed after click on the button New Item', function () {
         const correctItems = 'Passed';
         const wrongItems = 'Failed';
-        
+
         cy.get('a[href="/view/all/newJob"]')
             .should('have.text', 'New Item')
             .realClick()
-        
+
         cy.url().should('contain', '/view/all/newJob')
 
         cy.get('.j-item-options li span')
             .then(($els) => {
                 const arrayItems = Cypress.$.makeArray($els).map($el => $el.innerText);
                 if (arrayItems.toString() != newItemNames.projectNames.toString()) {
-                  return wrongItems
+                    return wrongItems
                 }
                 else {
-                  return correctItems 
+                    return correctItems
                 }
             })
             .should('deep.equal', correctItems)
@@ -64,20 +64,25 @@ describe('newItem', () => {
     });
 
     it('AT_05.08.011 | New Item > Verify Items Names', () => {
-        cy.get('#tasks [href="/view/all/newJob"]'). click()
+        cy.get('#tasks [href="/view/all/newJob"]').click()
         cy.get('.label').then($els => {
             return Cypress.$.makeArray($els).map($el => $el.innerText)
         })
-          .should('deep.equal', newItemNames.projectNames)
+            .should('deep.equal', newItemNames.projectNames)
     });
 
-    it ('AT_05.08.013 | Verify Names and Icons via tab New Item', function () {
+    it('AT_05.08.013 | Verify Names and Icons via tab New Item', function () {
 
         cy.get('[href="/view/all/newJob"]').click();
         cy.url().should('contain', '/view/all/newJob');
-        cy.get('.label').should('have.length', newItemNames.projectNames.length).each(($el,idx) => {
-        expect ($el.text()).to.be.equal(newItemNames.projectNames[idx]); 
-        cy.get ('.icon').should('have.length',6).should('be.visible')       
- })
-})
-})
+        cy.get('.label').should('have.length', newItemNames.projectNames.length).each(($el, idx) => {
+            expect($el.text()).to.be.equal(newItemNames.projectNames[idx]);
+            cy.get('.icon').should('have.length', 6).should('be.visible')
+        })
+    })
+    it.only('AT_05.08_015 | Verify Names and Icons are visible', () => {
+        cy.get('a[href="/view/all/newJob"]').click();
+        cy.url().should('include', '/view/all/newJob');
+        cy.get('.icon img').should('have.length', 6).should('be.visible');
+    });
+});
