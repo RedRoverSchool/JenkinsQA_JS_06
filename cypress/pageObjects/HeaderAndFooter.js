91425e42f19f75629da56dbeea04d9dfa2023ed2
IraKalinka <ira.boeing@gmail.com>
2023-06-01 23:22:37 -0700

    POM > Verify User Icon has dropdown menu

--- cypress/pageObjects/HeaderAndFooter.js
+++ cypress/pageObjects/HeaderAndFooter.js
@@ -4,10 +4,8 @@ class HeaderAndFooter {
     getUserNameLink = () => cy.get('div.login a[href*="user"]');
     getUserDropDownBtn = () => cy.get('div.login a[href*="user"] button');
     getUserConfigureMenu = () => cy.get('#breadcrumb-menu li a[href*="configure"] span');
-
-
-
-
+    getUserDropdownChevronBtn = () => cy.get('.login button');
+    getUserDropdownMenuItemsList = () => cy.get('.bd li');
 
 
 
@@ -22,11 +20,17 @@ class HeaderAndFooter {
         return new UserConfigurePage();
     }
 
+    clickUserDropdownChevronBtn() {
+        this.getUserDropdownChevronBtn().realHover().click();
+        return this;
+    }
 
-
-
-
-
-
+    getUserDropdownMenuItemList() {
+        return this
+        .getUserDropdownMenuItemsList()
+        .then($els => { 
+            return Cypress._.map($els, 'innerText')
+        }); 
+    }
 }
 export default HeaderAndFooter;
