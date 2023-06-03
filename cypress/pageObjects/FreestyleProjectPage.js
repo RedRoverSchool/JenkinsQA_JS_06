ac7f6d07b4881f663e1cf7bac2e7211e4e4fc081
Iryna <iryna.kolhanova@gmail.com>
2023-06-03 17:33:18 -0400

    refaxtor delete dublicated css selector

--- cypress/pageObjects/FreestyleProjectPage.js
+++ cypress/pageObjects/FreestyleProjectPage.js
@@ -2,7 +2,6 @@ import FreestyleProjectRenamePage from "./FreestyleProjectRenamePage";
 
 class FreestyleProjectPage {
     getRenameSideMenuLink = () => cy.get('#side-panel a[href$="rename"]');
-    getProjectName = () => cy.get('.job-index-headline');
     getFreestyleProjectHeader = () => cy.get('#main-panel h1');  
     
     clickRenameSideMenuLink() {
