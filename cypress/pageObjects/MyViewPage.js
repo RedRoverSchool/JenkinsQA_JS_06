0508a2861be4569cfecd8d3b63a1e4a543a6866f
Halyna A <7108581@gmail.com>
2023-06-04 13:50:57 -0700

    fixed code

--- cypress/pageObjects/MyViewPage.js
+++ cypress/pageObjects/MyViewPage.js
@@ -5,7 +5,7 @@ import PipelinePage from "./PipelinePage";
 class MyViewPage {
   getNewItemSideMenuLink = () => cy.get('a[href$="my-views/view/all/newJob"]');
   getBreadcrumbMyViewsItem = () => cy.get('li:nth-child(5) a:nth-child(1)');
-  getPipelineNameLink = () =>  cy.get('a[href="job/testPipeline/"]');
+  getPipelineNameLink = () =>  cy.get('a[href^="job/"].jenkins-table__link');
 
 
   clickNewItemSideMenuLink() {
