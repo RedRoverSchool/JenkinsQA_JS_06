95d3de78e0746ca96ef49b7e2aa6c820992d397f
Halyna A <7108581@gmail.com>
2023-06-04 08:40:28 -0700

    1

--- cypress/e2e/tests/myView.cy.js
+++ cypress/e2e/tests/myView.cy.js
@@ -4,14 +4,14 @@ import HomePage from "../../pageObjects/HomePage";
 import { freestyleProjectName } from "../../fixtures/pom_fixtures/newItemPage.json";
 import { headerText } from "../../fixtures/pom_fixtures/freestyleProjectPage.json";
 import {pipelineName} from "../../fixtures/pom_fixtures/newItemPage.json";
-import DashboardBradcrumbs from "../../pageObjects/DashboardBradcrumbs";
+import DashboardBreadcrumbs from "../../pageObjects/DashboardBreadcrumbs";
 import HeaderAndFooter from "../../pageObjects/HeaderAndFooter";
 
 
 describe('myView', () => {
 
   const homePage = new HomePage();
-  const dashboardBradcrambs = new DashboardBradcrumbs();
+  const dashboardBreadcrumbs = new DashboardBreadcrumbs();
   const headerAndFooter = new HeaderAndFooter();
      
     it('AT_09.08.001 | <My view> Create Freestyle Project job', () => {
@@ -27,16 +27,16 @@ describe('myView', () => {
     });
 
     it('AT_04.03_001|< My View> Verify that user can open selected Pipeline', () => {
-      dashboardBradcrambs
+        dashboardBreadcrumbs
           .clickNewItemBtn()
           .typeNewItemNameInputField(pipelineName)
           .selectPipelineItem()
           .clickNewItemOkBtn()
           .clickSaveBtn();
-      dashboardBradcrambs
+        dashboardBreadcrumbs
           .clickDashboardBtn();
 
-      headerAndFooter
+        headerAndFooter
           .clickUserDropDownBtn()
           .selectUserMyViewsMenu()
           .clickPipelineNameLink()
