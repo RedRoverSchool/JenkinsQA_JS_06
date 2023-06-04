5aba3d770c501509e76cc81cbc88498c2eccd540
Nina Kosova <ubuf.96@gmail.com>
2023-06-04 22:17:02 +0300

    refactor

--- cypress/e2e/tests/headerAndFooter.cy.js
+++ cypress/e2e/tests/headerAndFooter.cy.js
@@ -4,10 +4,12 @@ import HeaderAndFooter from "../../pageObjects/HeaderAndFooter";
 import {restAPIPageTitle} from "../../fixtures/pom_fixtures/restAPIPage.json"
 import {userDropdownMenuItems} from "../../fixtures/pom_fixtures/headerAndFooter.json";
 import resultSearchBox from "../../fixtures/pom_fixtures/resultSearchBox.json"
-import {inputText} from "../../fixtures/pom_fixtures/headerAndFooter.json"
+import {inputText} from "../../fixtures/pom_fixtures/headerAndFooter.json";
+import HomePage from "../../pageObjects/HomePage";
 describe('headerAndFooter', () => {
 
     const headerAndFooter = new HeaderAndFooter();
+    const homePage = new HomePage();
 
     it('AT_03.01.002 | Verify link Rest Api redirected to the page with correct header', () => {
         headerAndFooter
@@ -32,9 +34,14 @@ describe('headerAndFooter', () => {
     })
 
      it('AT_01.01_019 | Redirection to the homepage by label', () => {
-        headerAndFooter
-        .clickJenkinsHomeTitle()
-        .getHomePageLink()
-        .should('eq', `http://localhost:${Cypress.env('local.port')}/`)
+        homePage
+            .clickNewItemSideMenuLink()
+
+        headerAndFooter    
+            .clickJenkinsHomeLink() 
+            
+        homePage    
+            .getHomePageLink()
+            .should('eq', `http://localhost:${Cypress.env('local.port')}/`);
      });
 })
