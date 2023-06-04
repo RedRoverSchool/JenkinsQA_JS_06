2468e5902e2f868a2b460f3ae560d982cd854920
Halyna A <7108581@gmail.com>
2023-06-04 08:48:44 -0700

    AT_04.03_001|< My View> VerifyYserOpen selPipeline

--- cypress/pageObjects/DashboardBreadcrumbs.js
+++ cypress/pageObjects/DashboardBreadcrumbs.js
@@ -1,12 +1,36 @@
-
+import NewItemPage from "./NewItemPage";
+import PipelinePage from "./PipelinePage";
 class DashboardBreadcrumbs {
 
    getDashboardDropdownBtn = () => cy.get('#breadcrumbs a');
    getDashboardDropdownMenuItemsList = () => cy.get('#breadcrumb-menu>div:first-child>ul>li');
-
+   getDashboardBtn = () => cy.get('#breadcrumbBar li:first-child');
+   getProjectStatustable = () => cy.get('table#projectstatus');
+   getPipelineNameLink = () => cy.get('a[href="job/testPipeline/"]');
+   getNewItemBtn = () => cy.get('a[href="/view/all/newJob"]');
+   
    clickDashboardDropdownBtn() {
       this.getDashboardDropdownBtn().realHover().click('right');
       return this;
+   }
+
+   getDashboardDropdownMenuItems() {
+      return this.getDashboardDropdownMenuItemsList();
+   }
+
+   clickDashboardBtn() {
+      this.getDashboardBtn().realHover().click();
+      return this;
+   }
+
+   clickPipelineNameLink() {
+      this.getPipelineNameLink().click()
+      return new PipelinePage();
+   }
+
+   clickNewItemBtn() {
+      this.getNewItemBtn().realHover().click();
+      return new NewItemPage();
    }
 }
 export default DashboardBreadcrumbs;
