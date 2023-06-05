a571c9d8aa342b830d8d9d677517e16f818bf2fd
Alex Kushnir <classic.ua@gmail.com>
2023-06-05 14:38:00 -0700

    RF POM AT_15.04_003 | Folder | Delete folder from dashboard

--- cypress/e2e/tests/folder.cy.js
+++ cypress/e2e/tests/folder.cy.js
@@ -1,12 +1,14 @@
 /// <reference types="cypress" />
 
 import HomePage from "../../pageObjects/HomePage";
+import HeaderAndFooter from "../../pageObjects/HeaderAndFooter"
 import {folderName} from "../../fixtures/pom_fixtures/newItemPage.json";
 import {folderDescription} from "../../fixtures/pom_fixtures/folderPage.json";
 
 describe('folder', () => {
 
     const homePage = new HomePage();
+    const headerAndFooter = new HeaderAndFooter();
 
     it('AT_15.02.001 | Verify possibility to add folder description', () => {
         homePage
@@ -19,5 +21,22 @@ describe('folder', () => {
             .typeFolderDescription(folderDescription)
             .saveFolderDescription()
             .getFolderDescription().should('have.text', folderDescription);
+    });
+
+    it('AT_15.04_003 | Folder | Delete folder from dashboard', () => {
+        homePage
+            .clickNewItemSideMenuLink()
+            .selectFolderItem()
+            .typeNewItemNameInputField(folderName)
+            .clickOkBtnAndGoFolderConfig()
+            .clickSaveBtnAndGoFolder()
+        
+        headerAndFooter
+            .clickJenkinsHomeLink()
+            .clickProjectDrpDwnBtn()
+            .clickDeleteMultiBrPipelineFromDrpDwnMenu()
+            .clickSubmitBtn()
+            .getPageBody()
+            .should("not.have.text", folderName)
     });
 });
