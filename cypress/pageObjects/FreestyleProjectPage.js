254ce1657190846361f646b6a02efb42edbad040
Andrei Gaponenko <puhpuh690@gmail.com>
2023-06-06 09:30:44 +0300

    RF AT_12.06_001 "Disable project" option exists

--- cypress/pageObjects/FreestyleProjectPage.js
+++ cypress/pageObjects/FreestyleProjectPage.js
@@ -4,8 +4,9 @@ import FreestyleProjectConfigurePage from "./FreestyleProjectConfigurePage"
 class FreestyleProjectPage {
     getConfigureSideMenuLink = () => cy.get('a[href$="configure"]')
     getRenameSideMenuLink = () => cy.get('#side-panel a[href$="rename"]');
-    getFreestyleProjectHeader = () => cy.get('#main-panel h1');  
-    
+    getFreestyleProjectHeader = () => cy.get('#main-panel h1');
+    getDisableProjectBtn = () => cy.get('form#disable-project').find('button[name="Submit"]');
+
     clickConfigureSideMenuLink() {
         this.getConfigureSideMenuLink().click()
         return new FreestyleProjectConfigurePage()
@@ -13,7 +14,7 @@ class FreestyleProjectPage {
     clickRenameSideMenuLink() {
         this.getRenameSideMenuLink().click();
         return new FreestyleProjectRenamePage();
-    }    
+    }
 }
 
 export default FreestyleProjectPage;
