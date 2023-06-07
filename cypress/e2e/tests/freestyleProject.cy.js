b803d7db6694f01ea1ef0f9d26a8ec68f237e553
Andrei Gaponenko <42135041+puh50@users.noreply.github.com>
2023-06-07 14:51:40 +0300

    Update freestyleProject.cy.js

--- cypress/e2e/tests/freestyleProject.cy.js
+++ cypress/e2e/tests/freestyleProject.cy.js
@@ -62,7 +62,7 @@ describe('freestyleProject', () => {
     });
 
     it('AT_12.03_002 | Verify that using the same name an error message is appeared', function () {
-        cy.createFreestyleProject(newItemData.freestyleProjectName
+        cy.createFreestyleProject(newItemData.freestyleProjectName)
 
         homePage        
             .clickNamesProjects()
