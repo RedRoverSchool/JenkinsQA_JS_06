9d10c02fdb52a76e2690d55443b7d3e52efef63b
EkBlinova <katte.blinova@gmail.com>
2023-06-07 12:07:36 -0700

    [RF] AT_04.02_005 skip test

--- cypress/e2e/breadcrumbsMenu.cy.js
+++ cypress/e2e/breadcrumbsMenu.cy.js
@@ -48,6 +48,7 @@ describe('BreadcrumbsMenu', () => {
    })
 
    it.skip('AT_04.02_005 | Verify Dashboard is visible in the breadcrumb on every page and user can return to the main page.', () => {
+
       cy.get('.jenkins-breadcrumbs__list-item').contains('Dashboard').should('be.visible')
       cy.get('.jenkins-breadcrumbs__list-item').click()
       cy.get('.empty-state-block h1').should('have.text', 'Welcome to Jenkins!')
