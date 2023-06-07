938b585a9af441a3486cfc15a8e6a4e6a31027d8
nadilova <nadilova@gmail.com>
2023-06-07 02:39:56 +0200

    RF | POM > AT_02.06_004 | Homepage > Description input textarea does not exist

--- cypress/e2e/tests/homePage.cy.js
+++ cypress/e2e/tests/homePage.cy.js
@@ -3,11 +3,20 @@
 import HomePage from "../../pageObjects/HomePage";
 
 describe("homePage", () => {
+
     const homePage = new HomePage()
 
     it("AT_02.06_005 | Homepage > Verification of the button 'Add description'", () => {
         homePage
-          .clickAddDescriptionBtn();
-        cy.focused().should('have.attr', 'name', 'description') 
+          .clickAddDescriptionBtn()
+        cy.focused()
+          .should('have.attr', 'name', 'description') 
     })
+
+    it("AT_02.06_004 | Homepage > Description input textarea does not exist", () => {
+      homePage
+          .getAddDescriptionField()
+          .should('not.exist')
+  })
+
 })
