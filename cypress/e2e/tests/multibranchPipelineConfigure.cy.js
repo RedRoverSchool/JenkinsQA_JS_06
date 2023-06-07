ae403e22d3c550a4d2d11ee226cd5a2f27f3c5fd
Yulia Lopatko <engineeryulia@gmail.com>
2023-06-07 11:33:26 -0600

    fix assert

--- cypress/e2e/tests/multibranchPipelineConfigure.cy.js
+++ cypress/e2e/tests/multibranchPipelineConfigure.cy.js
@@ -70,12 +70,12 @@ describe('multibranchPipelineConfigure', () => {
             .should('deep.equal', multibranchPipelineConfigData.addSourceItemsList)
     });
 
-    it('AT_16.01_05 | Verify check boxes change color', () => {
+    it.only('AT_16.01_05 | Verify check boxes change color', () => {
         cy.createMultibranchPipelineProject(newItemData.multibranchPipelineName)
         homePage
             .clickMultibranchPipelineNameLink(newItemData.multibranchPipelineName)
             .clickConfigureTheProjectLink()
             .hoverScanTriggerCheckbox()
-            .should('be.visible', 'box-shadow')
+            .should('have.css', 'box-shadow')
     });
 });
