4ca81d30ffc6d22afbc36b80feaf718a72de8dae
SvetlanaKazakova <mks.kazakova@mail.ru>
2023-06-19 13:38:48 +0300

    test1

--- cypress/pageObjects/RestAPIPage.js
+++ cypress/pageObjects/RestAPIPage.js
@@ -1,7 +1,7 @@
 import RestApiDocPage from "./RestApiDocPage";
 class RestAPIPage {
     getRestApiTitle =() => cy.get('#main-panel h1');
-    getRestApiDocTitle =() => cy.get('[href="https://www.jenkins.io/redirect/remote-api"]');
+    getRestApiDocTitle =() => cy.get('[href="https://www.jenkins.io/redirect/remote-api"]').contains('the documentation');
 
     clickLinkTheDocumentation() {
         this.getRestApiDocTitle().click()
