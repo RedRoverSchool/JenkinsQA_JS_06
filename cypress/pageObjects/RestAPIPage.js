d2d3a303eb4ec01ce42270bade4f53d9b0866358
SvetlanaKazakova <107031108+SvetlanaKazakova@users.noreply.github.com>
2023-06-19 15:07:51 +0300

    Sk/apidocpage (#1003)
    * AT_03.01.002  Footer  open LinkThe documentation

--- cypress/pageObjects/RestAPIPage.js
+++ cypress/pageObjects/RestAPIPage.js
@@ -1,7 +1,13 @@
-
+import RestApiDocPage from "./RestApiDocPage";
 class RestAPIPage {
     getRestApiTitle =() => cy.get('#main-panel h1');
+    getRestApiDocTitle =() => cy.get('[href="https://www.jenkins.io/redirect/remote-api"]').contains('the documentation');
 
+    clickLinkTheDocumentation() {
+        this.getRestApiDocTitle().click()
+        return new RestApiDocPage()
+    }
+    
 
 
 
---
+++ cypress/pageObjects/RestApiDocPage.js
@@ -0,0 +1,6 @@
+class RestApiDocPage {
+getRestApiDocPageTitle =() => cy.get('h1#remote-access-api')
+}
+
+
+export default RestApiDocPage
